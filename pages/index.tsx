cd251efdab794fa47ae2dca3d77caf36a22d6d9f
kadet <techkadet@gmail.com>
2021-04-12 01:23:24 +0100

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import Head from "next/head";
 import { MainLayout } from "../layouts";
 
 const Home = () => {
-  return (<MainLayout>
-
-  </MainLayout>)
+  return (
+    <MainLayout>
+      <h1>Home Page Kwek</h1>
+    </MainLayout>
+  );
 };
 
 export default Home;
