32a83de4df86e194f1f5271a45710f0911051d66
kadet <techkadet@gmail.com>
2021-04-16 18:23:42 +0100

    added meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
           rel="stylesheet"
         />
+        <title>Kwek</title>
+        <meta name="description" content="Online Shop"/>
       </Head>
       <Component {...pageProps} />
       <div className="no-mobile">
