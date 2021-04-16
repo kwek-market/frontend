70f644aaf7c2b3bc7329b0d4f0d92c3c89b8051c
egbaidomeh james <egbaidomehjamez@gmail.com>
2021-04-16 02:19:21 -0700

    added the sellerLanding component

--- pages/sell/index.tsx
+++ pages/sell/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {MainLayout} from '../../layouts';
-import { HeroSection, FeatureSection, } from '../../components/sellerLanding'
+import { HeroSection, FeatureSection, Use } from '../../components/sellerLanding'
 
 function index() {
   return (
     <MainLayout>
      <HeroSection /> 
      <FeatureSection />
+     <Use />
     </MainLayout>
   )
 }
