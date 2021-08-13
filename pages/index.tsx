3e796290097221bcbc3544c5516d0096248f5479
Oluwaseye <seyepelumi@gmail.com>
2021-08-13 23:10:59 +0100

    made category grid responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import { useEffect } from 'react'
+import { useEffect } from "react";
 import { MainLayout } from "@/layouts";
-import { Hero, Features, CategoryGrid, Brands } from '@/components/home'
+import { Hero, Features, CategoryGrid, Brands } from "@/components/home";
 // import { userFetcherWithAuth } from '@/helpers'
 
-import { parseCookies } from 'nookies'
+import { parseCookies } from "nookies";
 // import { GET_USER } from '@/store/user/user.queries'
 
 const Home = ({ cookies }) => {
@@ -13,30 +13,36 @@ const Home = ({ cookies }) => {
 
   useEffect(() => {
     console.log(cookies);
-  }, [])
+  }, []);
 
   return (
     <MainLayout>
       <Hero />
       <Features />
       <CategoryGrid title="Deals Of The day" timer={true} cards={cards} />
-      <CategoryGrid title="Computer & Electronics" sidebar={true} banners={banners} />
-      <CategoryGrid title="Clothing & Apparel" sidebar={true} banners={banners} />
+      <CategoryGrid
+        title="Computer & Electronics"
+        sidebar={true}
+        banners={banners}
+      />
+      <CategoryGrid
+        title="Clothing & Apparel"
+        sidebar={true}
+        banners={banners}
+      />
       <Brands />
     </MainLayout>
   );
 };
 
-export async function getServerSideProps (ctx) {
+export async function getServerSideProps(ctx) {
   const cookies = parseCookies(ctx);
 
   return {
     props: {
-      cookies
-    }
-  }
-  
+      cookies,
+    },
+  };
 }
-
 
 export default Home;
