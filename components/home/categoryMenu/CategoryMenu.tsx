c8d285ebdd973d9e85f5520ff3501b4087db880f
Oluwaseye <seyepelumi@gmail.com>
2021-08-15 18:36:05 +0100

    made hero & category menu responsive

--- components/home/categoryMenu/CategoryMenu.tsx
+++ components/home/categoryMenu/CategoryMenu.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import styles from "./CategoryMenu.module.scss";
 
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
+import { useState } from "react";
 
 const CategoryMenu = () => {
+  const [showCat, setShowCat] = useState<boolean>(false);
+
   const categories: any[] = [
     { name: "Electronics" },
     { name: "Beauty & Health" },
@@ -17,22 +20,37 @@ const CategoryMenu = () => {
   ];
 
   return (
-    <ul id={styles.category_menu}>
+    <section id={styles.category_menu}>
       <div className={styles.category_menuTitle}>
-        <i className="fas fa-bars"></i>
+        <i className={`fas fa-bars ${styles.burger_icon}`}></i>
         <h4 className={styles.category_menuHeading}>All Categories</h4>
+        <div
+          className={styles.category_showMore_icon}
+          onClick={() => setShowCat(!showCat)}
+        >
+          <i className={`fas fa-chevron-right ${styles.chevron_down}`} style={{transform: showCat && "rotate(-90deg)"}}></i>
+        </div>
       </div>
-      {categories.map((item, index) => (
-        <li key={index} className={styles.menu_item}>
-          <Link href="/">
-            <a className={styles.menu_link}>
-              <Image src='/svg/cat-icon-electronics.svg' width='20' height='20'  />
-               <span className={styles.menu_text}> {item.name} </span>
-            </a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+      <ul
+        className={styles.category_list}
+        style={{ maxHeight: showCat && "325px" }}
+      >
+        {categories.map((item, index) => (
+          <li key={index} className={styles.menu_item}>
+            <Link href="/">
+              <a className={styles.menu_link}>
+                <Image
+                  src="/svg/cat-icon-electronics.svg"
+                  width="20"
+                  height="20"
+                />
+                <span className={styles.menu_text}> {item.name} </span>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
