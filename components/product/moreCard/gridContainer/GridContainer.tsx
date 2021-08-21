742051c8319d0854e40ff757dce5ed11698ffadf
Oluwaseye <seyepelumi@gmail.com>
2021-08-21 18:25:27 +0100

    made product item page responsive

--- components/product/moreCard/gridContainer/GridContainer.tsx
+++ components/product/moreCard/gridContainer/GridContainer.tsx
@@ -4,15 +4,18 @@ import styles from "./GridContainer.module.scss";
 import { TitleBlock, Card } from "../index";
 import { ProductBox } from "@/shared";
 
-const GridContainer = ({ title, cards }: any) => {
+interface GridContainerProps {
+  title: string;
+  cards?: any;
+}
+
+const GridContainer = ({ title, cards }: GridContainerProps) => {
   const products = [1, 2, 3, 4, 5];
 
   return (
     <div id={styles.categoryGrid}>
       <TitleBlock title={title} />
-      <div
-        className={styles.mainContainer__full}
-      >
+      <div className={styles.mainContainer__full}>
         <div className={styles.products}>
           {products.map((product: any) => (
             <div key={product} className={styles.product}>
