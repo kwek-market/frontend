eaf9e17c50e0f7c2c20174fc522670d58dee6fd3
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-10-27 12:14:01 +0100

    sign up setup complete

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
-import Router from "next/router";
+import { useRouter } from "next/router";
 
 import { AuthLayout } from "@/layouts";
-import AuthFormSignUp from "@/shared/authFormSignUp/AuthFormSignUp";
+import styles from "../shared/authForm/AuthForm.module.scss";
 
-// import { userFetcher } from "@/helpers";
-import { CREATE_USER } from "@/store/user/user.queries";
 import { setUser } from "@/store/user/user.actions";
 
 import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
+import Link from "next/link";
 
 const Page = ({ user, setUser }) => {
-  const createAccount = async (formData: any) => {
-    const query = CREATE_USER;
+  const [showPassword1, setShowPassword1] = useState<boolean>(false);
+  const [showPassword2, setShowPassword2] = useState<boolean>(false);
+  const [formData, setFormData] = useState<any>({});
+  const [error, setError] = useState<any>({});
+  const [loading, setLoading] = useState<any>(false);
+  const [createUserData, setCreateUserData] = useState<any>({});
 
-    const variables = {
-      email: formData.email,
-      fullName: formData.fullName,
-      password1: formData.password1,
-      password2: formData.password2,
-    };
+  const router = useRouter()
 
+
+  const createAccount = async (e: any, formData: any) => {
     // const data = await userFetcher(query, variables);
-    console.log("variables", variables);
+    e.preventDefault();
     const result = await fetch(`https://kwekapi.com/v1/kwekql`, {
       headers: {
         "Content-Type": `application/json`,
@@ -31,10 +32,10 @@ const Page = ({ user, setUser }) => {
       body: JSON.stringify({
         query: `
       mutation{
-        createUser(email: "${variables.email}", 
-          fullName: "${variables.fullName}",
-           password1: "${variables.password1}", 
-           password2: "${variables.password2}"){
+        createUser(email: "${formData.email}", 
+          fullName: "${formData.fullName}",
+           password1: "${formData.password1}", 
+           password2: "${formData.password2}"){
           status
           message
         }
@@ -44,70 +45,97 @@ const Page = ({ user, setUser }) => {
     });
 
     const { data } = await result.json();
-    // console.log(data);
-    // console.log(data.createUser.message)
+    const apis = data.createUser;
+    setCreateUserData(apis);
 
-    {
-      data.createUser &&
-        // Set user state
-        setUser({
-          ...user,
-          createUserMessage: data.createUser.message,
-          email: formData.email
-        });
-    }
-    if (
-      data.createUser !== null &&
-      data.createUser.message !==
+    console.log(apis);
+
+    const validateEmail =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (apis !== null &&
+      apis.message !==
         "Password should contain numerical character" &&
-      data.createUser.message !==
+      apis.message !==
         "Password is should not be less than 8 characters" &&
-      data.createUser.message !== "Password should contain numerical character"
+      apis.message !==
+        "Password should contain numerical character" &&
+      apis.message ===
+        `Successfully created account for ${apis.email}`
     ) {
-      setUser({
-        ...user,
-        createUserMessage: data.createUser.message,
-        email: formData.email,
-      });
-      Router.push("/verify-account");
+      setError({ status: true, message: apis.message, success: true });
+
+      router.push("/");
+    } else if (!formData.email && !formData.password) {
+      setError({ status: true, message: "Input your email and password", success: false });
+      setLoading(false);
     } else if (
-      data.createUser.message === `Successfully created account for ${data.createUser.email}`
+      formData.email &&
+      formData.password &&
+      !validateEmail.test(formData.email)
     ) {
-      Router.push("/verify-account");
-    } else {
-      setUser({
-        ...user,
-        createUserMessage: data.createUser.message,
-        email: formData.email,
+      setError({ status: true, message: "Invalid email", success: false });
+      setLoading(false);
+    } else if (!formData.email && formData.password) {
+      setError({ status: true, message: "type in your email", success: false });
+      setLoading(false);
+    } else if (!formData.password1) {
+      setLoading(false);
+      setError({ status: true, message: "Input your password", success: false });
+    } else if (formData.password1 !== formData.password2) {
+      setError({ status: true, message: "Passwords do not match", success: false });
+      setLoading(false);
+    } else if (apis.message === "Enter Valid E-mail") {
+      setError({ status: true, message: "Enter Valid E-mail", success: false });
+      setLoading(false);
+    } else if (
+      apis.message ===
+      "Password is should not be less than 8 characters"
+    ) {
+      setError({
+        status: true,
+        message: "Password is should not be less than 8 characters", success: false
       });
+      setLoading(false);
+    } else if (
+      apis.message === "Password should contain numerical character"
+    ) {
+      setError({
+        status: true,
+        message: "Password should contain numerical character", success: false
+      });
+      setLoading(false);
+    } else if (apis.message === "E-mail Already in use") {
+      setError({ status: true, message: "E-mail Already in use", success: false });
+      setLoading(false);
+    } else {
+      setError({ status: true, message: `${apis.message}`, success: true });
+      setLoading(true);
+      router.push("/");
     }
   };
-  console.log(user);
+
+
+  const handleSubmit = (e: any, submitData: any) => {
+    submitData.action(formData);
+    router.push("/");
+
+    // Email validation
+    const validateEmail =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    
+      if (createUserData.message === `Successfully created account for ${createUserData.email}`) {
+      setError({ status: true, message: `Successfully created account for ${createUserData.email}` });
+      submitData.action(formData);
+      console.log(formData);
+      router.push("/");
+
+    }
+  };
 
   const form = {
     title: "Create an Account",
-    fields: [
-      {
-        name: "fullName",
-        placeholder: "Full Name",
-        type: "text",
-      },
-      {
-        name: "email",
-        placeholder: "Email Address",
-        type: "email",
-      },
-      {
-        name: "password1",
-        placeholder: "Password",
-        type: "password",
-      },
-      {
-        name: "password2",
-        placeholder: "Confirm Password",
-        type: "password",
-      },
-    ],
     submit: {
       text: "Create Account",
       action: createAccount,
@@ -120,6 +148,7 @@ const Page = ({ user, setUser }) => {
     createUserInfo: {
       message: user.createUserMessage,
     },
+    subtitle: "",
   };
 
   const bannerText = {
@@ -128,15 +157,103 @@ const Page = ({ user, setUser }) => {
     lineThree: "Different",
   };
 
-  useEffect(() => {
-    if (user.id !== null) {
-      Router.push("/login");
-    }
-  });
-
   return (
     <AuthLayout id="createAccount" withBanner={true} bannerText={bannerText}>
-      <AuthFormSignUp {...form} />
+      <div id={styles.authForm}>
+        <form className={styles.form}>
+          <div className={styles.form_titleblock}>
+            <h2 className={styles.form_title}>Create an Account</h2>
+          </div>
+          {error && (
+            <span className={error.success ? `${styles.form_success}` : `${styles.form_error}`}>
+              <div>{error.message}</div>
+            </span>
+          )}
+
+          <div className={styles.form_inputContainer}>
+            <input
+              className={styles.form_input}
+              type={"text"}
+              onChange={(e) =>
+                setFormData({ ...formData, fullName: e.target.value })
+              }
+              placeholder={"Full Name"}
+            />
+          </div>
+          <div className={styles.form_inputContainer}>
+            <input
+              className={styles.form_input}
+              type={"email"}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              placeholder={"Email Address"}
+            />
+          </div>
+          <div className={styles.form_inputContainer}>
+            <input
+              className={styles.form_input}
+              type={
+                "password" ? (showPassword1 ? "text" : "password") : "password"
+              }
+              onChange={(e) =>
+                setFormData({ ...formData, password1: e.target.value })
+              }
+              placeholder={"Password"}
+            />
+            {"password" && (
+              <i
+                className={`${styles.form_inputIcon} ${
+                  !showPassword1 ? "far fa-eye" : "far fa-eye-slash"
+                }`}
+                onClick={() => setShowPassword1(!showPassword1)}
+              ></i>
+            )}
+          </div>
+          <div className={styles.form_inputContainer}>
+            <input
+              className={styles.form_input}
+              type={
+                "password" ? (showPassword2 ? "text" : "password") : "password"
+              }
+              onChange={(e) =>
+                setFormData({ ...formData, password2: e.target.value })
+              }
+              placeholder={"Confirm Password"}
+            />
+            {"password" && (
+              <i
+                className={`${styles.form_inputIcon} ${
+                  !showPassword2 ? "far fa-eye" : "far fa-eye-slash"
+                }`}
+                onClick={() => setShowPassword2(!showPassword2)}
+              ></i>
+            )}
+          </div>
+
+          <div className={styles.form_btnContainer}>
+            <button
+              className={`btn bg-primary ${styles.btn}`}
+              onClick={(e) => createAccount(e, formData)}
+            >
+              {loading && loading ? (
+                <Loader type="Puff" color="#fff" height={30} width={30} />
+              ) : (
+                form.submit.text
+              )}
+            </button>
+          </div>
+
+          {form.extra && (
+            <div className={styles.form_extra}>
+              <p className={styles.form_extraText}>{form.extra.text}</p>
+              <Link href={form.extra.linkUrl}>
+                <a className={styles.form_extraLink}>{form.extra.linkText}</a>
+              </Link>
+            </div>
+          )}
+        </form>
+      </div>
     </AuthLayout>
   );
 };
