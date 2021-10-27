6aee5fe8d7f577246f1f6401786b440f2b6c4678
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-10-27 07:54:54 +0100

    sign up setup

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,66 +1,88 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Router from "next/router";
 
 import { AuthLayout } from "@/layouts";
-import { AuthForm } from "@/shared";
+import AuthFormSignUp from "@/shared/authFormSignUp/AuthFormSignUp";
 
-import { userFetcher } from "@/helpers";
+// import { userFetcher } from "@/helpers";
 import { CREATE_USER } from "@/store/user/user.queries";
-import { setUser } from '@/store/user/user.actions'
+import { setUser } from "@/store/user/user.actions";
 
 import { connect } from "react-redux";
 
 const Page = ({ user, setUser }) => {
   const createAccount = async (formData: any) => {
     const query = CREATE_USER;
+
     const variables = {
       email: formData.email,
       fullName: formData.fullName,
-      password1: formData.password,
-      password2: formData.password,
+      password1: formData.password1,
+      password2: formData.password2,
     };
 
     // const data = await userFetcher(query, variables);
-
+    console.log("variables", variables);
     const result = await fetch(`https://kwekapi.com/v1/kwekql`, {
-        headers: {
-          "Content-Type": `application/json`,
-        },
-        method: "POST",
-        body: JSON.stringify({
-          query: `
+      headers: {
+        "Content-Type": `application/json`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        query: `
       mutation{
-        createUser(email: "eemmanuel.idoko@gmail.com", fullName: "Pidoxy" password1: "pidoxy.com1", password2: "pidoxy.com1"){
-          user {
-            id
-            firstName
-            lastName
-            email
-          }
+        createUser(email: "${variables.email}", 
+          fullName: "${variables.fullName}",
+           password1: "${variables.password1}", 
+           password2: "${variables.password2}"){
           status
           message
         }
       }          
       `,
-        }),
-      });
+      }),
+    });
 
-      const { data } = await result.json();
-      const apis = data;
+    const { data } = await result.json();
+    // console.log(data);
+    // console.log(data.createUser.message)
 
-    if (data.createUser.status) {
+    {
+      data.createUser &&
+        // Set user state
+        setUser({
+          ...user,
+          createUserMessage: data.createUser.message,
+          email: formData.email
+        });
+    }
+    if (
+      data.createUser !== null &&
+      data.createUser.message !==
+        "Password should contain numerical character" &&
+      data.createUser.message !==
+        "Password is should not be less than 8 characters" &&
+      data.createUser.message !== "Password should contain numerical character"
+    ) {
       setUser({
         ...user,
-        email: formData.email
-      })
-
+        createUserMessage: data.createUser.message,
+        email: formData.email,
+      });
       Router.push("/verify-account");
-    } else if (user.email) {
+    } else if (
+      data.createUser.message === `Successfully created account for ${data.createUser.email}`
+    ) {
       Router.push("/verify-account");
     } else {
-      console.log(data);
+      setUser({
+        ...user,
+        createUserMessage: data.createUser.message,
+        email: formData.email,
+      });
     }
   };
+  console.log(user);
 
   const form = {
     title: "Create an Account",
@@ -95,6 +117,9 @@ const Page = ({ user, setUser }) => {
       linkText: "Login",
       linkUrl: "/login",
     },
+    createUserInfo: {
+      message: user.createUserMessage,
+    },
   };
 
   const bannerText = {
@@ -111,7 +136,7 @@ const Page = ({ user, setUser }) => {
 
   return (
     <AuthLayout id="createAccount" withBanner={true} bannerText={bannerText}>
-      <AuthForm {...form} />
+      <AuthFormSignUp {...form} />
     </AuthLayout>
   );
 };
@@ -121,7 +146,7 @@ const mapStateToProps = (state: any) => ({
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  setUser: (user: any) => dispatch(setUser(user))
-})
+  setUser: (user: any) => dispatch(setUser(user)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Page);
