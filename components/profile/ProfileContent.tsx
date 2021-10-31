64a37131e4bc50967aacb833c78cc4869a8471b4
billmal071 <billmal071@gmail.com>
2021-10-31 23:09:41 +0100

    fix:fixed css issues

---
+++ components/profile/ProfileContent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Account, Addresses, Empty, Orders, Track } from "./content";
+
+function ProfileContent({ activeBtn }) {
+  const [menu, setMenu] = React.useState(false);
+
+  return (
+    <div className="tw-border tw-border-gray-500 tw-border-opacity-50 tw-rounded tw-p-5">
+      {/* <div className="tw-flex tw-justify-between"> */}
+        {activeBtn === "Account" && <Account activeBtn={activeBtn} />}
+        {activeBtn === "My Orders" && <Orders activeBtn={activeBtn} />}
+        {activeBtn === "Track My Order" && <Track activeBtn={activeBtn} />}
+        {/* {activeBtn === "Sell On Kwek" && <Account activeBtn={activeBtn} />} */}
+        {activeBtn === "My Addresses" && <Addresses activeBtn={activeBtn} />}
+      {/* </div> */}
+    </div>
+  );
+}
+
+export default ProfileContent;
