f75edacb44bb781a93fa6ad2f65e19d3eb32651b
Obi Fortune <gabrielobi.of@gmail.com>
2021-11-30 01:40:29 +0200

    fixed step issue in react step builder

--- components/verification/ProgressTracker.tsx
+++ components/verification/ProgressTracker.tsx
@@ -1,35 +1,43 @@
-import React from "react";
-import Style from "./tracker.module.scss";
+import React from 'react';
+import { NavigationComponentProps } from 'react-step-builder';
+import Style from './tracker.module.scss';
 
-function ProgressTracker() {
+interface progressTrackerProps extends NavigationComponentProps {}
+
+const ProgressTracker: React.FC<progressTrackerProps> = props => {
+  const { allSteps, current: currentStep } = props;
+
   return (
     <section className="tw-bg-white-100 tw-py-10 tw-px-2 md:tw-px-8 tw-flex tw-justify-between tw-rounded-md">
-      <div
-        className={`tw-flex tw-flex-col tw-items-center tw-flex-1 tw-text-center tw-relative`}
-      >
-        <div className="tw-absolute tw-left-[115px] tw-top-[12px] tw-border-b-2 tw-border-dotted tw-border-gray-kwek100 tw-opacity-50 tw-w-[86%] "></div>
-        <div
-          className={`tw-border-2 tw-border-solid tw-border-brown-kwek400 tw-rounded-full tw-h-7 tw-w-7`}
-        ></div>
-        <p className="tw-text-xs tw-font-medium tw-mt-2">Vendor's Policy</p>
-      </div>
-
-      <div className="tw-flex tw-flex-col tw-items-center tw-flex-1 tw-text-center">
-        <div className="tw-border-2 tw-border-solid tw-border-brown-kwek400 tw-rounded-full tw-h-7 tw-w-7"></div>
-        <p className="tw-text-xs tw-font-medium tw-mt-2">Valid ID</p>
-      </div>
-
-      <div className="tw-flex tw-flex-col tw-items-center tw-flex-1 tw-text-center">
-        <div className="tw-border-2 tw-border-solid tw-border-brown-kwek400 tw-rounded-full tw-h-7 tw-w-7"></div>
-        <p className="tw-text-xs tw-font-medium tw-mt-2">Verify BVN</p>
-      </div>
-
-      <div className="tw-flex tw-flex-col tw-items-center tw-flex-1 tw-text-center">
-        <div className="tw-border-2 tw-border-solid tw-border-brown-kwek400 tw-rounded-full tw-h-7 tw-w-7"></div>
-        <p className="tw-text-xs tw-font-medium tw-mt-2">Verify Bank Account</p>
-      </div>
+      {allSteps.map(({ title }, index) => {
+        const iconTheme =
+          index >= currentStep
+            ? 'tw-border-brown-kwek400'
+            : 'tw-border-green-success tw-bg-green-success';
+        const textTheme = index < currentStep && 'tw-text-green-success';
+        const barTheme =
+          index >= currentStep
+            ? 'tw-border-gray-kwek400'
+            : 'tw-border-green-success tw-bg-green-success';
+        return (
+          <>
+            <div
+              key={index}
+              className="tw-flex tw-flex-col tw-items-center tw-flex-1 tw-text-center"
+            >
+              <div
+                className={`tw-border-2 tw-border-solid tw-rounded-full tw-h-7 tw-w-7${iconTheme}`}
+              ></div>
+              <p className={`tw-text-xs tw-font-medium tw-mt-2 ${textTheme}`}>
+                {title}
+              </p>
+            </div>
+            {/* <div className="tw-border-b-2 tw-border-dotted tw-border-gray-kwek100 tw-opacity-50 tw-w-[100%]"></div> */}
+          </>
+        );
+      })}
     </section>
   );
-}
+};
 
 export default ProgressTracker;
--- pages/sell/verification/index.tsx
+++ pages/sell/verification/index.tsx
@@ -4,25 +4,37 @@ import {
   VendorsPolicy,
   VerifyBankAccount,
   VerifyBVN,
-} from "@/components/verification";
-import VerificationLayout from "@/layouts/seller/VerificationLayout";
-import { RootState } from "@/store/rootReducer";
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Steps, Step, StepComponentProps } from "react-step-builder";
+} from '@/components/verification';
+import VerificationLayout from '@/layouts/seller/VerificationLayout';
+import { RootState } from '@/store/rootReducer';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Steps, Step, StepComponentProps } from 'react-step-builder';
 
 function index(props: StepComponentProps) {
   const dispatch = useDispatch();
   const seller = useSelector((state: RootState) => state.seller);
 
+  interface configProps {
+    navigation: {
+      component: (props: any) => JSX.Element;
+      location: 'before' | 'after';
+    };
+  }
+  const config: configProps = {
+    navigation: {
+      component: ProgressTracker, // a React component with special props provided automatically
+      location: 'before', // or after
+    },
+  };
+
   return (
     <VerificationLayout>
-      <ProgressTracker />
-      <Steps>
-        <Step title="vendor's policy" component={VendorsPolicy} />
-        <Step title="upload a valid id" component={ValidID} />
-        <Step title="verify bvn" component={VerifyBVN} />
-        <Step title="verify bank account" component={VerifyBankAccount} />
+      <Steps config={config}>
+        <Step title="Vendor's Policy" component={VendorsPolicy} />
+        <Step title="Upload a Valid ID" component={ValidID} />
+        <Step title="Verify BVN" component={VerifyBVN} />
+        <Step title="Verify Bank Account" component={VerifyBankAccount} />
       </Steps>
     </VerificationLayout>
   );
