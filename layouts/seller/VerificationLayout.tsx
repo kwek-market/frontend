aa4de25f3fd1b02c3770766e26cdd520f9ea9c93
Obi Fortune <gabrielobi.of@gmail.com>
2021-12-02 13:39:14 +0200

    fixed id page and setup the image uploading

--- layouts/seller/VerificationLayout.tsx
+++ layouts/seller/VerificationLayout.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Header } from "@/shared/verification";
+import React from 'react';
+import { Header } from '@/shared/verification';
 
 function VerificationLayout({ children }) {
   return (
-    <div className="tw-bg-primary">
+    <div className="tw-bg-primary tw-h-screen">
       <Header />
       <div className="tw-px-5 md:tw-px-14 tw-py-5">{children}</div>
     </div>
