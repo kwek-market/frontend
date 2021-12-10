df2a4ff69405edffe576dff853746651d0625418
Favvie <favourprecious3@gmail.com>
2021-12-10 17:04:59 +0100

    review page UI: filled reviews update

---
+++ components/seller/reviews/ReviewItem.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./reviews.module.scss";
+import Image from "next/image";
+
+function ReviewItem({ name, date, reviewBody, reviewerPic, sellerPic }) {
+	return (
+		<div className={styles.reviewer}>
+			<Image src={reviewerPic} width="80" height="80" className={styles.img} />
+			<div className={styles.reviewer_content}>
+				<div className={styles.user}>
+					<div className={styles.infouser}>
+						<p className={styles.name}>{name}</p>
+						<p className={styles.date}>{date}</p>
+					</div>
+					<div className={styles.star_container}>
+						<span className={`material-icons ${styles.star_filled}`}>star</span>
+						<span className={`material-icons ${styles.star_filled}`}>star</span>
+						<span className={`material-icons ${styles.star_filled}`}>star</span>
+						<span className={`material-icons ${styles.star_filled}`}>star</span>
+						<span className={`material-icons ${styles.star_filled}`}>star</span>
+					</div>
+				</div>
+				<p className={styles.review_body}>{reviewBody}</p>
+				<div className={styles.seller_reply}>
+					<Image
+						src={sellerPic}
+						width="24"
+						height="24"
+						className={styles.img}
+					/>
+					<input
+						type="text"
+						placeholder="Write a reply..."
+						className={styles.reply_box}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ReviewItem;
