b4db4f9617627e0b1e11ae7726dc4718dd9c4071
Favvie <favourprecious3@gmail.com>
2021-12-23 08:16:25 +0100

    reviewItem update

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,43 +1,60 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
-import ReviewList from '../components/seller/reviews/ReviewList';
-import { MainLayout } from '@/layouts';
-import styles from '../components/seller/reviews/reviews.module.scss';
+import ReviewList from "../components/seller/reviews/ReviewList";
+import { MainLayout } from "@/layouts";
+import styles from "../components/seller/reviews/Reviews.module.scss";
 
 const Page = function () {
-  return (
-    <MainLayout title="Reviews">
-      <h2 className={styles.heading}>Reviews</h2>
+	return (
+		<MainLayout title="Reviews">
+			<h2 className={styles.heading}>Reviews</h2>
 
-      <p className={styles.description}>store performance</p>
+			<p className={styles.description}>store performance</p>
 
-      <hr />
+			<hr />
 
-      <div className={styles.container}>
-        <ReviewList reviewHeading="successful sales" salesNumber="0" salesAmount="This month - NGN 0" />
-        <ReviewList reviewHeading="product quality" salesNumber="0%" salesAmount="This month - NGN 0%" />
-        <ReviewList reviewHeading="delivery rate" salesNumber="0%" salesAmount="This month - NGN 0%" />
-        <ReviewList reviewHeading="communication" salesNumber="0%" salesAmount="This month - NGN 0%" />
-      </div>
+			<div className={styles.container}>
+				<ReviewList
+					reviewHeading="successful sales"
+					salesNumber="0"
+					salesAmount="This month - NGN 0"
+				/>
+				<ReviewList
+					reviewHeading="product quality"
+					salesNumber="0%"
+					salesAmount="This month - NGN 0%"
+				/>
+				<ReviewList
+					reviewHeading="delivery rate"
+					salesNumber="0%"
+					salesAmount="This month - NGN 0%"
+				/>
+				<ReviewList
+					reviewHeading="communication"
+					salesNumber="0%"
+					salesAmount="This month - NGN 0%"
+				/>
+			</div>
 
-      <div className={styles.flex1}>
-        <p className={styles.description}>product review</p>
+			<div className={styles.flex1}>
+				<p className={styles.description}>product review</p>
 
-        <div className={styles.flex}>
-          Sort by:{' '}
-          <div className={styles.recent}>
-            recent <span className={`material-icons ${styles.star_filled}`}>star</span>
-          </div>
-        </div>
-      </div>
+				<div className={styles.flex}>
+					Sort by:{" "}
+					<div className={styles.recent}>
+						recent{" "}
+						<span className={`material-icons ${styles.star_filled}`}>star</span>
+					</div>
+				</div>
+			</div>
 
-      <hr />
-      <div className={styles.emptyReviews}>
-        <p>You currently have no reviews</p>
-      </div>
-    </MainLayout>
-  );
+			<hr />
+			<div className={styles.emptyReviews}>
+				<p>You currently have no reviews</p>
+			</div>
+		</MainLayout>
+	);
 };
 
 export default Page;
