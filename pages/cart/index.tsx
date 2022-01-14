458e9a03c8499d23654dcd83c775185ed672347c
Williams <billmal071@gmail.com>
2022-01-14 10:48:13 +0100

    fix:billing module scss naming issue fixed

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
-import { CartGrid } from '@/components/cart';
+import { CartEmpty, CartGrid } from "@/components/cart";
 
-import ExtraInfo from '@/shared/extraInfo/ExtraInfo';
-import { MainLayout } from '@/layouts';
+import ExtraInfo from "@/shared/extraInfo/ExtraInfo";
+import { MainLayout } from "@/layouts";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/rootReducer";
 
 const Page = function () {
+  const { user, cart } = useSelector((state: RootState) => state);
+
+  const isLoading = cart.loading && (
+    <div className="text-center">
+      <div className="spinner-border text-primary" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+
+  const hasError = cart.error && (
+    <div className="text-center">
+      <p className="text-danger">{cart.error}</p>
+    </div>
+  );
+
+  const hasCart =
+    cart.cart && cart.cart.length > 0 ? <CartGrid /> : <CartEmpty />;
+
   return (
     <MainLayout title="Cart">
-      <CartGrid />
+      {isLoading}
+      {hasError}
+      {hasCart}
       <ExtraInfo />
     </MainLayout>
   );
