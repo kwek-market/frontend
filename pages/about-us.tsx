e7ad1ab2ca150e1e33a6de14bc0bc07e1c052ec2
Michellebuchiokonicha <michelleokonicha@gmail.com>
2022-01-15 17:03:41 +0100

    About Us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
+
+import  AboutUs  from'@/components/aboutUs/AboutUs'
 
 import { MainLayout } from '@/layouts';
 
+
 const Page = function () {
-  return <MainLayout title="About Us" />;
+    return (
+        <MainLayout title= 'About Us'>
+            <AboutUs />
+        </MainLayout>
+    );
 };
 
 export default Page;
