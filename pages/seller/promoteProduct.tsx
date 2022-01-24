58e6a1f0cec34496691ab2853b592be8e6ed6720
Michellebuchiokonicha <michelleokonicha@gmail.com>
2022-01-24 13:28:48 +0100

    single product page, promote page, return policy page, about us in footer, sitemap in footer

---
+++ pages/seller/promoteProduct.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PromoteHeader from '@/shared/PromoteHeader/PromoteHeader';
+import PromoteProduct from '@/components/promoteProduct/PromoteProduct';
+
+
+
+const Filled = function () {
+    return(
+        <div>
+           <PromoteHeader> 
+            <PromoteProduct /> 
+              </PromoteHeader> 
+        </div>
+    )
+}
+
+export default Filled
