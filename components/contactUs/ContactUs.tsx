6cd87a3afdaf7714cece8943a7c0eebed1c146ad
Michelle Buchi Okonicha <michelleokonicha@gmail.com>
2022-01-25 22:30:44 +0100

    tabs (#73)

---
+++ components/contactUs/ContactUs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from "next/image";
+import styles from './ContactUs.module.scss'
+
+const ContactUs = function () {
+    return (
+        <div></div>
+    )
+}
+
+
+export default ContactUs
