dc9615e069f388346ccaf509c1bdea33c8cb51c3
Michelle Buchi Okonicha <michelleokonicha@gmail.com>
2022-01-27 22:10:51 +0100

    Mimi (#79)
    * tabs
    
    * menu dropdow, product filled responsiveness, store options responsiveness, single product responsiveness
    
    * profile, promote, menu dropdown, single product responsiveness, product filled responsiveness

--- components/singleProduct/SingleProduct.tsx
+++ components/singleProduct/SingleProduct.tsx
@@ -6,8 +6,6 @@ import styles from './SingleProduct.module.scss'
 const SingleProduct = function () {
     return (
         <div className={styles.product}>
-            
-                 
 
                 <div className={styles.holder}>
                     <div className={styles.key}>
