13ffa7316b185d13062146ef3e2f5f0d99ffe940
billmal071 <billmal071@gmail.com>
2022-02-16 00:18:13 +0100

    feat:sellers product page

--- components/singleProduct/SingleProduct.tsx
+++ components/singleProduct/SingleProduct.tsx
@@ -2,138 +2,108 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./SingleProduct.module.scss";
+import { useRouter } from "next/router";
+import StarRatingComponent from "react-star-rating-component";
+import { v4 } from "uuid";
 
-const SingleProduct = function () {
+const SingleProduct = function ({ product, setShowProduct }) {
+  const router = useRouter();
+
+  function promote(id: string) {
+    router.push(`/seller/promote-product/${id}`);
+  }
+
+  function edit() {
+    router.push("#");
+  }
+
   return (
     <div className={styles.product}>
       <div className={styles.holder}>
         <div className={styles.key}>
-          <Image
-            src="/images/key.png"
-            width="30"
-            height="20"
-            className={styles.key}
+          <i
+            className="fas fa-arrow-left tw-cursor-pointer"
+            onClick={() => setShowProduct(false)}
           />
         </div>
         <div className={styles.greenly}>
-          <div className={styles.green}>
-            <Link href="/">
-              <a className={styles.link}>
-                <Image
-                  src="/images/buttonGreen.png"
-                  width="149"
-                  height="64"
-                  className={styles.green}
-                />
-              </a>
-            </Link>
-          </div>
-          <div className={styles.white}>
-            <Link href="/">
-              <a className={styles.link}>
-                <Image
-                  src="/images/buttonWhite.png"
-                  width="110"
-                  height="64"
-                  className={styles.white}
-                />
-              </a>
-            </Link>
-          </div>
+          <button
+            className="tw-bg-green-success tw-text-white-100 tw-rounded-sm tw-py-2 tw-px-3"
+            onClick={() => promote(product.id)}
+          >
+            Promote
+          </button>
+          <button
+            className="tw-border-2 tw-border-gray-kwek900 tw-rounded-sm tw-py-2 tw-px-3"
+            onClick={() => edit()}
+          >
+            Edit
+          </button>
         </div>
       </div>
       <div className={styles.single}>
         <div className={styles.images}>
           <div className={styles.mask}>
             <Image
-              src="/images/mask.png"
+              src={product.image[0].imageUrl}
               width="722"
               height="693.41"
               className={styles.img}
             />
           </div>
           <div className={styles.masky}>
-            <div>
+            {product.image.map((image) => (
               <Image
-                src="/images/masky.png"
+                key={v4()}
+                src={image.imageUrl}
                 width="163.22"
                 height="156.54"
                 className={styles.img}
               />
-            </div>
-            <div>
-              <Image
-                src="/images/masky.png"
-                width="163.22"
-                height="156.54"
-                className={styles.img}
-              />
-            </div>
-            <div>
-              <Image
-                src="/images/masky.png"
-                width="163.22"
-                height="156.54"
-                className={styles.img}
-              />
-            </div>
-            <div>
-              <Image
-                src="/images/masky.png"
-                width="163.22"
-                height="156.54"
-                className={styles.img}
-              />
-            </div>
+            ))}
           </div>
         </div>
 
         <div className={styles.element}>
           <div>
-            <h4>Women's Fashion Shiny High Heels</h4>
+            <h4>{product.productTitle}</h4>
             <div className={styles.review}>
-              <p>
-                <b>Product Code:</b> MODA124323
-              </p>
+              <div className="tw-flex-1">
+                <strong>Product Code:</strong> {product.id}
+              </div>
 
               <div className={styles.revs}>
-                <div className={styles.box_productRating}>
-                  <span className="material-icons">star</span>
-                  <span className="material-icons">star</span>
-                  <span className="material-icons">star</span>
-                  <span className="material-icons">star</span>
-                  <span className="material-icons">star</span>
-                </div>
-                <p className={styles.six}>(6 Reviews)</p>
+                <StarRatingComponent
+                  name="seller-product"
+                  starCount={5}
+                  value={product?.productRating[0]?.rating}
+                  editing={false}
+                  emptyStarColor="#c4c4c4"
+                  starColor="#ffc107"
+                />
+                <p className={styles.six}>
+                  ({product?.productRating?.likes} Reviews)
+                </p>
               </div>
             </div>
           </div>
-          <h2>$25.00</h2>
-          <p className={styles.name}>
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
-          </p>
+          <h2>₦{product.options[0].price}</h2>
+          <p className={styles.name}>{product.shortDescription}</p>
 
           <div className={styles.colors}>
             <p>COLOR: </p>
             <div className={styles.paint}>
-              <div className={styles.yellow}></div>
-              <div className={styles.maroon}></div>
-              <div className={styles.pink}></div>
+              <div className="tw-w=[32px] tw-h=[32px]">{product.color}</div>
             </div>
           </div>
           <div className={styles.size}>
             <p>SIZE:</p>
             <div className={styles.fort}>
-              <div className={styles.forty}>40</div>
-              <div className={styles.fortyone}>41</div>
-              <div className={styles.fortythree}>43</div>
-              <div className={styles.fortyfour}>44</div>
-              <div className={styles.fortyfive}>45</div>
+              {product.options.map((option) => (
+                <div key={v4()} className={styles.forty}>
+                  {option.size}
+                </div>
+              ))}
             </div>
           </div>
 
