e2869a8040c2258d000340abc91d8ca26ccdb824
billmal071 <billmal071@gmail.com>
2022-02-22 17:42:47 +0100

    feat:updated the product page and added rating feature

--- store/review/review.reducer.ts
+++ store/review/review.reducer.ts
@@ -1,73 +1,38 @@
-import reviewTypes  from "./review.types"; 
-
+import reviewTypes from "./review.types";
 
 const initialState = {
-    id: null,
-    loading: false,
-    error: null,
-    email: "",
-    status: false,
-    message: null,
-    productReview: {
-        id: null,
-        product: "",
-        rating: "",
-        review: null,
-        token: null,
-        vote: null,
-        email: null,
-        name: null,
-        comment: null
-    }
-
+  id: null,
+  loading: false,
+  error: null,
+  email: "",
+  status: false,
+  message: null,
 };
 
 export default function reviewReducer(
-    state: typeof initialState = initialState,
-    action: { type: any; payload: any}
+  state: typeof initialState = initialState,
+  action: { type: any; payload: any }
 ) {
-    switch (action.type) {
-        case reviewTypes.REVIEW_PRODUCT: 
-        return{
-            ...state,
-            loading: false,
-            email :action.payload.email,
-            error: null,
-        };
-        case reviewTypes.PRODUCT_RATING:
-            return {
-                ...state,
-                loading: false,
-                error: null,
-                rating: action.payload.rating
-            };
-        case reviewTypes.REVIEW_LOADING:
-            return{
-                  ...state,
-                  loading: true,
-            };
-            case reviewTypes.REVIEW_LOADING:
-            return{
-                  ...state,
-                  loading: true,
-                  error: null,
-                  name: action.payload.name
-            };
-            case reviewTypes.REVIEW_COMMENT:
-                return {
-                    ...state,
-                    loading: false,
-                    error: null,
-                    comment: action.payload.comment
-                };
-                case reviewTypes.ERROR:
-                    return {
-                      ...state,
-                      loading: false,
-                      error: action.payload,
-                    };
-        default:
-         return state;
-    };
-   
+  switch (action.type) {
+    case reviewTypes.REVIEW_PRODUCT:
+      return {
+        ...state,
+        loading: false,
+        email: action.payload.email,
+        error: null,
+      };
+    case reviewTypes.REVIEW_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case reviewTypes.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
 }
