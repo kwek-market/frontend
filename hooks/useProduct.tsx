32df41144a887c2637d1f9483dfdae65007d56bf
billmal071 <billmal071@gmail.com>
2022-02-23 00:37:44 +0100

    feat:category, product and payment done

--- hooks/useProduct.tsx
+++ hooks/useProduct.tsx
@@ -1,13 +1,43 @@
 import { userFetcher } from "@/helpers";
+import { ProductType } from "@/interfaces/commonTypes";
 import { GetProduct } from "@/store/product/product.queries";
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { QueryClient, useQuery } from "react-query";
 
 function useProduct(payload: { id: string }) {
-  return useQuery(["product"], () => userFetcher(GetProduct, payload), {
-    staleTime: Infinity,
-    cacheTime: 1000 * 60 * 20,
-    enabled: payload.id !== undefined,
+  const queryClient = new QueryClient();
+  const { id } = payload;
+  const [productData, setProductData] = useState({
+    loading: false,
+    error: null,
+    product: {} as ProductType,
   });
+
+  useEffect(() => {
+    if (payload.id === undefined) return;
+    (async () => {
+      try {
+        setProductData({ ...productData, loading: true });
+        const data = await queryClient.fetchQuery(["product"], () =>
+          userFetcher(GetProduct, payload)
+        );
+        setProductData({
+          ...productData,
+          error: null,
+          loading: false,
+          product: data.product,
+        });
+      } catch (err) {
+        setProductData({ ...productData, loading: false, error: err });
+      }
+    })();
+
+    return () => {
+      queryClient.cancelQueries(["product"]);
+    };
+  }, [id]);
+
+  return productData;
 }
 
 export default useProduct;
