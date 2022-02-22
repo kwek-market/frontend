4267b3079f718d510dfe643a5ea9b2323d524632
billmal071 <billmal071@gmail.com>
2022-02-22 15:26:51 +0100

    fix:moved placeOrder to order-confirmation

--- hooks/usePaymentVerify.tsx
+++ hooks/usePaymentVerify.tsx
@@ -1,28 +1,16 @@
 import { userFetcherWithAuth } from "@/helpers";
-import { PlaceOrder, VerifyPaymentType } from "@/interfaces/commonTypes";
+import { VerifyPaymentType } from "@/interfaces/commonTypes";
 import { VERIFYPAYMENT } from "@/store/billing/paymentLink.queries";
 import { message } from "antd";
-import React from "react";
 import { useMutation } from "react-query";
-import usePlaceOrder from "./usePlaceOrder";
 
-function usePaymentVerify(token: string, ref: string | string[]) {
-  const { mutate } = usePlaceOrder(token);
-  const txRef = ref;
-  console.log(txRef);
-
+function usePaymentVerify(token: string) {
   return useMutation(
     (data: VerifyPaymentType) =>
       userFetcherWithAuth(VERIFYPAYMENT, data, token),
     {
       onSuccess: (res) => {
-        const store = window.sessionStorage.getItem("order");
-        const order = JSON.parse(store);
-        console.log(txRef);
-        order.paymentRef = txRef;
-        console.log(order);
         message.success(res.verifyPayment.message);
-        mutate(order);
       },
       onError: (error) => {
         message.error((error as any).message);
