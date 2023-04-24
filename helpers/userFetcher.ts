dea3b9bf9450d6786c2f8cc88f8d259652bf3cf3
Nwokoye Chigozie <24975761+NwokoyeChigozie@users.noreply.github.com>
2023-04-24 16:08:47 +0100

    updated env

--- helpers/userFetcher.ts
+++ helpers/userFetcher.ts
@@ -2,7 +2,7 @@ import { GraphQLClient, request } from "graphql-request";
 
 // const oldUrl = "https://kwekapi.com/v1/kwekql";
 // const newUrl = "https://kwekapi.herokuapp.com/v1/kwekql";
-const graphqlEndpoint = process.env.BACKEND_ENDPOINT;
+const graphqlEndpoint = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT;
 
 export const userFetcher = async (query: string, variables?: any) => {
   return await request(graphqlEndpoint, query, variables);
