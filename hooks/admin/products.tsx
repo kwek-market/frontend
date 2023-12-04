4c7b4759dc5ff3d0173d44541a3d509bf74dba09
Psami-wondah <okechukwusamuel16@gmail.com>
2023-12-04 17:59:37 +0100

    started category page

--- hooks/admin/products.tsx
+++ hooks/admin/products.tsx
@@ -1,11 +1,21 @@
 import { userFetcherWithAuth, userFetcher } from "@/helpers";
-import { GET_PRODUCT, GET_PRODUCTS } from "@/store/admin/admin.queries";
+import {
+  GET_PRODUCT,
+  GET_PRODUCTS,
+  GET_PRODUCT_REVIEWS,
+} from "@/store/admin/admin.queries";
 import { useQuery } from "react-query";
 
 export type PayloadType = {
   pageSize: number;
   page: number;
   token?: string;
+};
+export type ReviewPayloadType = {
+  pageSize: number;
+  page: number;
+  productId: string;
+  sortBy: string;
 };
 
 export const useGetProducts = (payload: PayloadType) => {
@@ -17,5 +27,11 @@ export const useGetProducts = (payload: PayloadType) => {
 export const useGetProduct = (payload: { id: string }) => {
   return useQuery(["product-admin", payload], () =>
     userFetcher(GET_PRODUCT, payload)
+  );
+};
+
+export const useGetProductReviews = (payload: ReviewPayloadType) => {
+  return useQuery(["product-reviews-admin", payload], () =>
+    userFetcher(GET_PRODUCT_REVIEWS, payload)
   );
 };
