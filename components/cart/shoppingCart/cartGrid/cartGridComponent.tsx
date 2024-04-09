12f4e8e104bee3fefc2d967584c7552ec7c61810
kingsley-sunny <ezeobisunny51@gmail.com>
2024-04-09 15:16:39 +0100

    Added empty string as a fallback value and removed the Next_product_fallback_id

--- components/cart/shoppingCart/cartGrid/cartGridComponent.tsx
+++ components/cart/shoppingCart/cartGrid/cartGridComponent.tsx
@@ -72,7 +72,7 @@ const CartGridComponent = function () {
                   Seller:{" "}
                   {item.product?.user?.sellerprofile
                     ? item.product?.user?.sellerprofile[0]?.shopName
-                    : ''}
+                    : ""}
                 </p>
                 <p className={styles.name}>{item.product.productTitle}</p>
               </div>
