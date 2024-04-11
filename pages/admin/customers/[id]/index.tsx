a1a1293720da90b56bb2414127d8b95614bf9a64
kingsley-sunny <ezeobisunny51@gmail.com>
2024-04-11 16:49:24 +0100

    featue: implemented the endpoints in the /admin/customer/[id] page

--- pages/admin/customers/[id]/index.tsx
+++ pages/admin/customers/[id]/index.tsx
@@ -1,54 +1,73 @@
 import BreadCrumbs from "@/components/admin/breadcrumbs";
+import CustomerDetail from "@/components/admin/customers/customer-detail";
+import { FormHead } from "@/components/admin/form";
 import AdminTable from "@/components/table";
 import { AdminLayout } from "@/layouts";
-import { Divider, Tabs } from "antd";
+import { Empty, Tabs } from "antd";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
-import { FormHead } from "@/components/admin/form";
-import CustomerDetail from "@/components/admin/customers/customer-detail";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Load from "../../../../components/Loader/Loader";
+import { PAGE_SIZE } from "../../../../constants/constants";
+import { useGetCustomerOrders, useGetCustomerTotalOrders } from "../../../../hooks/admin/customers";
+import { userGetUserById } from "../../../../hooks/admin/user";
+import { RootState } from "../../../../store/rootReducer";
 
 const Customer = () => {
   const router = useRouter();
   const { TabPane } = Tabs;
   const [activeKey, setActiveKey] = useState("1");
-  const cardData = [
-    {
-      title: "Total No of Orders",
-      content: "17",
-    },
-    {
-      title: "Average Order Value",
-      content: "N14,500",
-    },
-    {
-      title: "Total Spent",
-      content: "N359,634",
-    },
-  ];
+
+  const { user } = useSelector((state: RootState) => state);
+
+  const [page, setPage] = useState(1);
+
+  const {
+    data: totalOrders,
+    isLoading: isLoadingTotalOrders,
+    error,
+  } = useGetCustomerTotalOrders({
+    id: router.query.id as string,
+    token: user.token,
+  });
+
+  const {
+    data: orders,
+    isLoading: isOrdersLoading,
+    error: ordersError,
+  } = useGetCustomerOrders({
+    page,
+    pageSize: PAGE_SIZE,
+    token: user.token,
+    id: router.query.id as string,
+  });
+
+  const {
+    data: customerData,
+    isLoading: isLoadingCustomerData,
+    error: customerDataError,
+  } = userGetUserById({
+    id: router.query.id as string,
+    token: user.token,
+  });
 
   const columns = [
     {
       title: "Order Number",
-      dataIndex: "order_number",
+      dataIndex: "orderId",
       key: "order_number",
-      render: (order_number) => (
+      render: order_number => (
         <Link
-          href={
-            "/admin/customers/" +
-            router.query?.id +
-            "/order-detail/" +
-            "order-" +
-            order_number
-          }
+          href={"/admin/customers/" + router.query?.id + "/order-detail/" + "order-" + order_number}
         >
-          <a className=" tw-text-black-kwek100">{order_number}</a>
+          <a className=' tw-text-black-kwek100'>{order_number}</a>
         </Link>
       ),
     },
     {
       title: "Order Date",
-      dataIndex: "order_date",
+      dataIndex: "dateCreated",
       key: "order_date",
     },
     {
@@ -58,57 +77,18 @@ const Customer = () => {
     },
     {
       title: "Status",
-      dataIndex: "status",
+      dataIndex: "deliveryStatus",
       key: "status",
     },
     {
       title: "Amount",
-      dataIndex: "amount",
+      dataIndex: "orderPriceTotal",
       key: "amount",
     },
     {
       title: "Payment",
-      dataIndex: "payment",
+      dataIndex: "paid",
       key: "payment",
-    },
-  ];
-
-  const data = [
-    {
-      key: "1",
-      order_number: "#37812",
-      order_date: "13/03/2023",
-      no_of_items: "14",
-      status: "Shipped",
-      amount: "N13,849",
-      payment: "Paid",
-    },
-    {
-      key: "2",
-      order_number: "#37812",
-      order_date: "13/03/2023",
-      no_of_items: "14",
-      status: "Shipped",
-      amount: "N13,849",
-      payment: "Paid",
-    },
-    {
-      key: "3",
-      order_number: "#37812",
-      order_date: "13/03/2023",
-      no_of_items: "14",
-      status: "Shipped",
-      amount: "N13,849",
-      payment: "Paid",
-    },
-    {
-      key: "4",
-      order_number: "#37812",
-      order_date: "13/03/2023",
-      no_of_items: "14",
-      status: "Shipped",
-      amount: "N13,849",
-      payment: "Paid",
     },
   ];
 
@@ -123,63 +103,99 @@ const Customer = () => {
             path: "/admin/customers/" + router.query?.id,
           },
         ]}
-        header="Customer Info"
-        buttonPath=""
-        buttonText="Send Email"
+        header='Customer Info'
+        buttonPath=''
+        buttonText='Send Email'
       />
 
-      <div className=" tw-mt-12 tw-font-poppins">
-        <CustomerDetail
-          image="/images/pp.png"
-          name="Akomolafe Akadri"
-          email="theakomolafeakadri@email.com"
-          phone="0812 345 6789"
-        />
-        <div className=" tw-pt-6  tw-w-[50vw] tw-grid tw-grid-cols-3 tw-gap-x-4">
-          {cardData.map((item, index) => (
-            <div
-              key={index}
-              className=" tw-py-6 tw-px-4 tw-bg-[#FAFBFF] tw-border tw-border-[#D7DCE0] tw-rounded-[20px] tw-font-dm-sans"
-            >
-              <p className=" tw-mb-0 tw-text-sm tw-text-[#3A434B]">
-                {item.title}
-              </p>
-              <p className="tw-mb-0 tw-pt-2 tw-text-[#0D0F11] tw-font-bold tw-text-2xl">
-                {item.content}
+      <div className=' tw-mt-12 tw-font-poppins'>
+        {/* THE CUSTOMER DETAILS */}
+
+        {isLoadingCustomerData ? <Load className='tw-h-64' /> : null}
+        {customerData?.getUserById ? (
+          <CustomerDetail
+            image='/images/pp.png'
+            name={customerData?.getUserById?.fullName}
+            email={customerData?.getUserById?.email}
+            phone={customerData?.getUserById?.phoneNumber}
+          />
+        ) : null}
+
+        {/* THE ORDER ANALYTICS */}
+        <div className=' tw-pt-6  tw-w-[50vw] tw-grid tw-grid-cols-3 tw-gap-x-4'>
+          {isLoadingTotalOrders ? <Load /> : null}
+          {totalOrders?.getCustomerOrders ? (
+            <div className=' tw-py-6 tw-px-4 tw-bg-[#FAFBFF] tw-border tw-border-[#D7DCE0] tw-rounded-[20px] tw-font-dm-sans'>
+              <p className=' tw-mb-0 tw-text-sm tw-text-[#3A434B]'>Total No of Orders</p>
+              <p className='tw-mb-0 tw-pt-2 tw-text-[#0D0F11] tw-font-bold tw-text-2xl'>
+                {totalOrders?.getCustomerOrders?.totalOrders}
               </p>
             </div>
-          ))}
+          ) : null}
+
+          <div className=' tw-py-6 tw-px-4 tw-bg-[#FAFBFF] tw-border tw-border-[#D7DCE0] tw-rounded-[20px] tw-font-dm-sans'>
+            <p className=' tw-mb-0 tw-text-sm tw-text-[#3A434B]'>Average Order Value</p>
+            <p className='tw-mb-0 tw-pt-2 tw-text-[#0D0F11] tw-font-bold tw-text-2xl'>14</p>
+          </div>
+
+          <div className=' tw-py-6 tw-px-4 tw-bg-[#FAFBFF] tw-border tw-border-[#D7DCE0] tw-rounded-[20px] tw-font-dm-sans'>
+            <p className=' tw-mb-0 tw-text-sm tw-text-[#3A434B]'>Total Spent</p>
+            <p className='tw-mb-0 tw-pt-2 tw-text-[#0D0F11] tw-font-bold tw-text-2xl'>14</p>
+          </div>
         </div>
-        <div className=" tw-py-4">
+        <div className=' tw-py-4'>
           <Tabs
             animated
             tabBarStyle={{ borderColor: "red" }}
-            className="adminTab"
+            className='adminTab'
             activeKey={activeKey}
-            onTabClick={(key) => setActiveKey(key)}
+            onTabClick={key => setActiveKey(key)}
           >
-            <TabPane tab="Order History" key="1">
-              <AdminTable data={data.slice(0, 4)} columns={columns} />
+            <TabPane tab='Order History' key='1'>
+              {isOrdersLoading ? <Load className='h-64' /> : null}
+              {orders?.getCustomerOrdersPaginated?.objects?.slice(0, 4) ? (
+                <AdminTable
+                  data={orders?.getCustomerOrdersPaginated?.objects}
+                  columns={columns}
+                  numberOfPages={orders?.getCustomerOrdersPaginated.pages}
+                  page={orders?.getCustomerOrdersPaginated.page}
+                  goToNext={() => {
+                    if (orders?.getCustomerOrdersPaginated?.hasNext) setPage(page + 1);
+                  }}
+                  goToPrev={() => {
+                    if (orders?.getCustomerOrdersPaginated?.hasPrev) setPage(page - 1);
+                  }}
+                  goToPage={page => {
+                    setPage(page);
+                  }}
+                />
+              ) : null}
             </TabPane>
           </Tabs>
         </div>
 
-        <Link href={"/admin/customers/" + router.query?.id + "/order-list"}>
-          <a className=" tw-underline tw-text-[#009D19] ">View All 17 Orders</a>
-        </Link>
+        {orders?.getCustomerOrdersPaginated?.objects?.length > 0 ? (
+          <Link href={"/admin/customers/" + router.query?.id + "/order-list"}>
+            <a className=' tw-underline tw-text-[#009D19] '>View All Orders</a>
+          </Link>
+        ) : null}
 
-        <FormHead>Address</FormHead>
-        <div className=" tw-pt-6 tw-space-y-6">
-          {Array(2)
-            .fill(null)
-            .map((item, index) => (
-              <div key={index}>
-                <p className="tw-mb-0 tw-font-medium">Maryjane Egbu</p>
-                <p className="tw-mb-0 tw-font-light tw-pt-4 tw-text-[#3A434B]">
-                  Random Federation 115302, Moscow ul. Varshavskaya, 15-2-178
-                </p>
-              </div>
-            ))}
+        <FormHead>Billing Addresses</FormHead>
+        <div className=' tw-pt-6 tw-space-y-6'>
+          {customerData?.getUserById?.billingSet?.length > 0
+            ? customerData?.getUserById?.billingSet.map((billing, index) => (
+                <div key={index}>
+                  <p className='tw-mb-0 tw-font-medium'>{billing?.fullName}</p>
+                  <p className='tw-mb-0 tw-font-light tw-pt-4 tw-text-[#3A434B]'>
+                    {billing?.address} {billing.state}
+                  </p>
+                </div>
+              ))
+            : null}
+
+          {customerData?.getUserById?.billingSet?.length === 0 ? (
+            <Empty description='No billing address found' />
+          ) : null}
         </div>
       </div>
     </AdminLayout>
