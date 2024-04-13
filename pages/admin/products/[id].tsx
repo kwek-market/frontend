8e83ad2b9448a429b2263965f1c9ef963f44a569
Ezeobi Kingsley Sunny <76210133+kingsley-sunny@users.noreply.github.com>
2024-04-13 12:32:24 +0100

    Sunny/admin (#138)
    * fixed Cart page not showing and added the correct link to the share butotn
    
    * implemented authorization in the admin page
    
    * implemented the create category ( but encountered an error from the backend)
    
    * implemented the search and delete categories
    
    * fixed: implemented the edit-category
    
    * implemented the endpoint in the  order page
    
    * fixed build error
    
    * Added empty string as a fallback value and removed the Next_product_fallback_id
    
    * feature: Added switch to admin at the footer if the user is an admin
    
    * implemented pagination when fetching customers
    
    * featue: implemented the endpoints in the /admin/customer/[id] page
    
    * feature: consumed the api endpoints when viewing a single orders
    
    * feature: added more product section when viewing a single product
    
    * worked on the product details page, and implemented the remining apis
    
    * fixed Cart page not showing and added the correct link to the share butotn
    
    * Added empty string as a fallback value and removed the Next_product_fallback_id
    
    * feature: added more product section when viewing a single product

--- pages/admin/products/[id].tsx
+++ pages/admin/products/[id].tsx
@@ -1,15 +1,14 @@
+import Load from "@/components/Loader/Loader";
 import BreadCrumbs from "@/components/admin/breadcrumbs";
-import { AdminLayout } from "@/layouts";
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import { Divider } from "antd";
-import { Tabs } from "antd";
 import OrderHistory from "@/components/admin/products/order-history";
 import Reviews from "@/components/admin/products/reviews";
-import { useGetProduct, useGetProductReviews } from "@/hooks/admin/products";
-import Load from "@/components/Loader/Loader";
+import { useGetProduct } from "@/hooks/admin/products";
+import { AdminLayout } from "@/layouts";
+import { Tabs } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const ProductDetail = () => {
   const { TabPane } = Tabs;
@@ -21,13 +20,6 @@ const ProductDetail = () => {
 
   const { data, isFetching } = useGetProduct({
     id: id as string,
-  });
-
-  const getProductReviews = useGetProductReviews({
-    page: 1,
-    pageSize: 10,
-    productId: id as string,
-    sortBy: "id",
   });
 
   return (
@@ -42,12 +34,12 @@ const ProductDetail = () => {
           },
         ]}
       />
-      <div className=" tw-mt-6 tw-font-poppins tw-flex tw-gap-x-4">
+      <div className=' tw-mt-6 tw-font-poppins tw-flex tw-gap-x-4'>
         {isFetching ? (
           <Load />
         ) : (
           <>
-            <div className="tw-min-w-[77px] tw-min-h-[99px]">
+            <div className='tw-min-w-[77px] tw-min-h-[99px]'>
               <Image
                 width={72}
                 height={72}
@@ -55,28 +47,24 @@ const ProductDetail = () => {
                   data?.product?.image[0]?.imageUrl ||
                   "https://res.cloudinary.com/psami-wondah/image/upload/v1701686786/tadukzulu3bmj4wzrszr.webp"
                 }
-                alt="hh"
-                objectFit="cover"
-                className="tw-rounded-[10px] tw-flex-shrink-0"
+                alt='hh'
+                objectFit='cover'
+                className='tw-rounded-[10px] tw-flex-shrink-0'
               />
             </div>
             <div>
-              <h2 className="tw-mb-0 tw-font-bold tw-text-lg">
-                {data?.product?.productTitle}
-              </h2>
-              <h3 className="tw-mb-0 tw-font-semibold">
+              <h2 className='tw-mb-0 tw-font-bold tw-text-lg'>{data?.product?.productTitle}</h2>
+              <h3 className='tw-mb-0 tw-font-semibold'>
                 Price: &nbsp;
-                <span className="tw-text-[#009D19]">
+                <span className='tw-text-[#009D19]'>
                   N{Number(data?.product?.options[0]?.price).toLocaleString()}
                 </span>
               </h3>
-              <div className="tw-flex tw-flex-col tw-text-xs tw-text-gray-kwek300a ">
+              <div className='tw-flex tw-flex-col tw-text-xs tw-text-gray-kwek300a '>
                 <div>
                   Seller:{" "}
-                  <Link
-                    href={`/admin/vendors/vendor-info/${data?.product?.user?.id}`}
-                  >
-                    <a className=" tw-text-[#AF1328] tw-underline">
+                  <Link href={`/admin/vendors/vendor-info/${data?.product?.user?.id}`}>
+                    <a className=' tw-text-[#AF1328] tw-underline'>
                       {data?.product?.user?.fullName}
                     </a>
                   </Link>
@@ -88,34 +76,63 @@ const ProductDetail = () => {
           </>
         )}
       </div>
-      <div className=" tw-font-poppins tw-mt-11">
+
+      <div className=' tw-font-poppins tw-mt-11'>
         <Tabs
           animated
           tabBarStyle={{ borderColor: "red" }}
-          className="adminTab"
+          className='adminTab'
           activeKey={activeKey}
-          onTabClick={(key) => setActiveKey(key)}
+          onTabClick={key => setActiveKey(key)}
         >
-          <TabPane tab="Order History" key="1">
+          <TabPane tab='Order History' key='1'>
             <OrderHistory />
           </TabPane>
           <TabPane
             tab={
-              <div className=" tw-flex tw-gap-x-3">
+              <div className=' tw-flex tw-gap-x-3'>
                 Reviews
-                <span className=" tw-rounded-full tw-bg-[#009D19] tw-w-5 tw-h-5 tw-flex tw-items-center tw-justify-center tw-text-white-100">
+                <span className=' tw-rounded-full tw-bg-[#009D19] tw-w-5 tw-h-5 tw-flex tw-items-center tw-justify-center tw-text-white-100'>
                   {reviewsCount}
                 </span>
               </div>
             }
-            key="2"
+            key='2'
           >
-            <Reviews
-              productId={id as string}
-              setReviewsCount={setReviewsCount}
-            />
+            <Reviews productId={id as string} setReviewsCount={setReviewsCount} />
           </TabPane>
         </Tabs>
+      </div>
+
+      <div className=' tw-grid tw-gap-x-8 tw-text-[#574240] tw-mt-8'>
+        <div className=' tw-bg-review tw-rounded-2xl tw-p-8'>
+          <h2 className='tw-mb-0 tw-text-2xl tw-font-semibold tw-text-[#574240]'>
+            More Product Information
+          </h2>
+
+          <div className='lg:tw-grid tw-gap-x-6 lg:tw-grid-cols-2 '>
+            <div className=' tw-bg-white-100 tw-p-6 tw-rounded-[10px] tw-mt-6'>
+              <p className=' tw-font-semibold tw-mb-0'>Brand</p>
+              <p className=' tw-text-opacity-70 tw-pt-2 tw-mb-0'>{data?.product?.brand}</p>
+            </div>
+            <div className=' tw-bg-white-100 tw-p-6 tw-rounded-[10px] tw-mt-6'>
+              <p className=' tw-font-semibold tw-mb-0'>Weight</p>
+              <p className=' tw-text-opacity-70 tw-pt-2 tw-mb-0'>{data?.product?.productWeight}</p>
+            </div>
+            <div className=' tw-bg-white-100 tw-p-6 tw-rounded-[10px] tw-mt-6'>
+              <p className=' tw-font-semibold tw-mb-0'>Warranty</p>
+              <p className=' tw-text-opacity-70 tw-pt-2 tw-mb-0'>{data?.product?.warranty}</p>
+            </div>
+            <div className=' tw-bg-white-100 tw-p-6 tw-rounded-[10px] tw-mt-6'>
+              <p className=' tw-font-semibold tw-mb-0'>Color</p>
+              <p className=' tw-text-opacity-70 tw-pt-2 tw-mb-0'>{data?.product?.color}</p>
+            </div>
+            <div className=' tw-bg-white-100 tw-p-6 tw-rounded-[10px] tw-mt-6'>
+              <p className=' tw-font-semibold tw-mb-0'>Gender</p>
+              <p className=' tw-text-opacity-70 tw-pt-2 tw-mb-0'>{data?.product?.gender}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </AdminLayout>
   );
