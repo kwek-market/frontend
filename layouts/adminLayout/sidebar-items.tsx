d1453bf98beff4576b2cf36e20ff87431107964b
kingsley-sunny <ezeobisunny51@gmail.com>
2024-05-01 22:11:15 +0100

    update: added a link to go back to the homepage from the admin page

--- layouts/adminLayout/sidebar-items.tsx
+++ layouts/adminLayout/sidebar-items.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
 import AdsIcon from "@/components/icons/admin/sidebar/ads";
 import CategoriesIcon from "@/components/icons/admin/sidebar/categories";
 import CustomersIcon from "@/components/icons/admin/sidebar/customers";
 import DashboardIcon from "@/components/icons/admin/sidebar/dashboard";
 import MarketingIcon from "@/components/icons/admin/sidebar/marketing";
+import OrderIcon from "@/components/icons/admin/sidebar/orders";
 import ProductsIcon from "@/components/icons/admin/sidebar/products";
 import SettingsIcon from "@/components/icons/admin/sidebar/settings";
 import TransactionsIcon from "@/components/icons/admin/sidebar/transactions";
 import VendorsIcon from "@/components/icons/admin/sidebar/vendors";
-import { useRouter } from "next/router";
+import { ChevronDownIcon, UserIcon } from "@heroicons/react/solid";
 import Link from "next/dist/client/link";
-import { ChevronDownIcon } from "@heroicons/react/solid";
-import OrderIcon from "@/components/icons/admin/sidebar/orders";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const SidebarItems = () => {
   const router = useRouter();
@@ -108,68 +108,75 @@ const SidebarItems = () => {
   const [items, setItems] = useState(paths);
 
   return (
-    <div className="tw-space-y-1">
-      {items.map((item, index) =>
-        item.options.length > 0 ? (
-          <div
-            key={index}
-            className={`  tw-text-sm tw-cursor-pointer `}
-            onClick={() => {
-              setItems((prev) => {
-                let tempPrev = prev.slice();
-                tempPrev[index].open = !tempPrev[index].open;
-                return tempPrev;
-              });
-            }}
-          >
+    <div className=''>
+      <div className='tw-space-y-1'>
+        {items.map((item, index) =>
+          item.options.length > 0 ? (
             <div
-              className={`tw-border-l-4 tw-pl-7  tw-border-transparent tw-py-3 tw-flex tw-items-center tw-gap-x-[10px] ${
-                checkActive(item.path)
-                  ? "tw-text-[#FABA00] tw-bg-[#D9D9D9] tw-bg-opacity-20 tw-border-l-white-100"
-                  : " tw-text-white-100 tw-text-opacity-70"
-              } `}
+              key={index}
+              className={`  tw-text-sm tw-cursor-pointer `}
+              onClick={() => {
+                setItems(prev => {
+                  let tempPrev = prev.slice();
+                  tempPrev[index].open = !tempPrev[index].open;
+                  return tempPrev;
+                });
+              }}
             >
-              <item.icon
-                fill={checkActive(item.path) ? "#FABA00" : "#ffffffb3"}
-              />
-              {item.name}
-              <ChevronDownIcon className=" tw-w-4 tw-h-4" />
-            </div>
-            <div
-              className={` tw-pl-7  tw-overflow-hidden tw-transition-[max-height] tw-duration-300 tw-ease-out ${
-                item.open ? "tw-max-h-40" : "tw-max-h-0"
-              }`}
-            >
-              <div className=" tw-pt-4 tw-flex tw-flex-col tw-gap-y-4 tw-pl-8">
-                {item.options.map((option, idx) => (
-                  <Link href={option.path} key={idx}>
-                    <a className="tw-text-white-100 tw-text-opacity-70">
-                      {option.name}
-                    </a>
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        ) : (
-          <Link href={item.path} passHref key={index}>
-            <div
-              className={` tw-border-l-4 tw-border-transparent tw-py-3 tw-pl-7 tw-text-sm tw-cursor-pointer ${
-                checkActive(item.path)
-                  ? "tw-text-[#FABA00] tw-bg-[#D9D9D9] tw-bg-opacity-20 tw-border-l-white-100"
-                  : " tw-text-white-100 tw-text-opacity-70"
-              }`}
-            >
-              <div className=" tw-flex tw-items-center tw-gap-x-[10px]">
-                <item.icon
-                  fill={checkActive(item.path) ? "#FABA00" : "#ffffffb3"}
-                />
+              <div
+                className={`tw-border-l-4 tw-pl-7  tw-border-transparent tw-py-3 tw-flex tw-items-center tw-gap-x-[10px] ${
+                  checkActive(item.path)
+                    ? "tw-text-[#FABA00] tw-bg-[#D9D9D9] tw-bg-opacity-20 tw-border-l-white-100"
+                    : " tw-text-white-100 tw-text-opacity-70"
+                } `}
+              >
+                <item.icon fill={checkActive(item.path) ? "#FABA00" : "#ffffffb3"} />
                 {item.name}
+                <ChevronDownIcon className=' tw-w-8 tw-h-8' />
+              </div>
+              <div
+                className={` tw-pl-7  tw-overflow-hidden tw-transition-[max-height] tw-duration-300 tw-ease-out ${
+                  item.open ? "tw-max-h-40" : "tw-max-h-0"
+                }`}
+              >
+                <div className=' tw-pt-4 tw-flex tw-flex-col tw-gap-y-4 tw-pl-8'>
+                  {item.options.map((option, idx) => (
+                    <Link href={option.path} key={idx}>
+                      <a className='tw-text-white-100 tw-text-opacity-70'>{option.name}</a>
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
-          </Link>
-        )
-      )}
+          ) : (
+            <Link href={item.path} passHref key={index}>
+              <div
+                className={` tw-border-l-4 tw-border-transparent tw-py-3 tw-pl-7 tw-text-sm tw-cursor-pointer ${
+                  checkActive(item.path)
+                    ? "tw-text-[#FABA00] tw-bg-[#D9D9D9] tw-bg-opacity-20 tw-border-l-white-100"
+                    : " tw-text-white-100 tw-text-opacity-70"
+                }`}
+              >
+                <div className=' tw-flex tw-items-center tw-gap-x-[10px]'>
+                  <item.icon fill={checkActive(item.path) ? "#FABA00" : "#ffffffb3"} />
+                  {item.name}
+                </div>
+              </div>
+            </Link>
+          )
+        )}
+      </div>
+
+      <Link href={"/"}>
+        <a
+          className={`tw-block tw-mt-8 tw-border-l-4 tw-border-transparent tw-py-3 tw-pl-7 tw-text-sm tw-cursor-pointer  tw-text-white-100 tw-text-opacity-70`}
+        >
+          <div className=' tw-flex tw-items-center tw-gap-x-[10px]'>
+            <UserIcon className='tw-w-4 tw-h-4' fill={"#ffffffb3"} />
+            Switch to User
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
