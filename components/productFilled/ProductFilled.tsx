a867262a25c30213338ec1e61fa87f46ef4823c5
kingsley-sunny <ezeobisunny51@gmail.com>
2024-05-07 20:10:55 +0100

    fix: fixed the product errors

--- components/productFilled/ProductFilled.tsx
+++ components/productFilled/ProductFilled.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Image from "next/image";
-import styles from "./ProductFilled.module.scss";
-import { ProductType } from "@/interfaces/commonTypes";
 import useAvgRating from "@/hooks/useAvgRating";
 import usePromotions from "@/hooks/usePromotions";
+import { ProductType } from "@/interfaces/commonTypes";
 import { RootState } from "@/store/rootReducer";
-import { useSelector } from "react-redux";
 import { Rate } from "antd";
+import Image from "next/image";
+import React from "react";
 import ReactPaginate from "react-paginate";
+import { useSelector } from "react-redux";
+import styles from "./ProductFilled.module.scss";
 
 type ProductFilledProps = {
   product: ProductType[];
@@ -34,7 +34,7 @@ const ProductFilled = function ({
   const { status, data, error } = usePromotions(token);
 
   function isPromoted(id: string) {
-    return data?.getSellerPromotedProducts.find((item) => item.id === id);
+    return data?.getSellerPromotedProducts.find(item => item.id === id);
   }
 
   function displayProductDetails(prod: any) {
@@ -43,59 +43,51 @@ const ProductFilled = function ({
   }
 
   return (
-    <section className="tw-shadow-lg">
+    <section className='tw-shadow-lg'>
       <div className={styles.filled}>
         <div className={styles.products}>
-          <div className="tw-text-lg tw-font-semibold">
-            All Products - {product.length}
-          </div>
-          <div className="tw-flex tw-gap-x-4 tw-items-center">
-            <p className="tw-p-0 tw-m-0">Sort by: </p>
-            <select
-              className=""
-              value={filter}
-              onChange={(e) => setFilter(e.target.value)}
-            >
-              <option value="-clicks">Most Popular</option>
-              <option value="-date_created">Recent</option>
-              <option value="sales">Price: Low to High</option>
-              <option value="-sales">Price: High to Low</option>
-              <option value="-rating">Product Rating</option>
+          <div className='tw-text-lg tw-font-semibold'>All Products - {product.length}</div>
+          <div className='tw-flex tw-gap-x-4 tw-items-center'>
+            <p className='tw-p-0 tw-m-0'>Sort by: </p>
+            <select className='' value={filter} onChange={e => setFilter(e.target.value)}>
+              <option value='-clicks'>Most Popular</option>
+              <option value='-date_created'>Recent</option>
+              <option value='sales'>Price: Low to High</option>
+              <option value='-sales'>Price: High to Low</option>
+              <option value='-rating'>Product Rating</option>
             </select>
           </div>
         </div>
-        <div className="tw-grid tw-gap-3 tw-grid-cols-2 lg:tw-grid-cols-4 tw-mt-2">
+        <div className='tw-grid tw-gap-3 tw-grid-cols-2 lg:tw-grid-cols-4 tw-mt-2'>
           {product.map((prod: ProductType) => (
             <div
               key={prod.id}
-              className="tw-shadow-lg tw-cursor-pointer tw-relative"
+              className='tw-shadow-lg tw-cursor-pointer tw-relative'
               onClick={() => displayProductDetails(prod)}
             >
               <Image
                 src={prod.image[0].imageUrl}
                 alt={prod.productTitle}
-                layout="responsive"
+                layout='responsive'
                 width={200}
                 height={200}
-                className=" tw-object-cover"
+                className=' tw-object-cover'
               />
               {!!isPromoted(prod.id) && (
-                <span className="tw-bg-green-success tw-text-white-100 tw-rounded-xl tw-py-1 tw-px-4 tw-absolute tw-left-[35%] md:tw-left-[60%] tw-top-[5%]">
+                <span className='tw-bg-green-success tw-text-white-100 tw-rounded-xl tw-py-1 tw-px-4 tw-absolute tw-left-[35%] md:tw-left-[60%] tw-top-[5%]'>
                   promoted
                 </span>
               )}
-              <div className="tw-flex tw-flex-wrap tw-justify-between tw-p-3">
+              <div className='tw-flex tw-flex-wrap tw-justify-between tw-p-3'>
                 <div>
-                  <p className="tw-text-lg tw-font-semibold tw-mb-0">
-                    {prod.productTitle}
-                  </p>
+                  <p className='tw-text-lg tw-font-semibold tw-mb-0'>{prod.productTitle}</p>
                   <Rate allowHalf value={useAvgRating(prod)} />
                 </div>
                 <div>
-                  <p className="tw-text-lg tw-font-semibold tw-mb-0 tw-text-right">
-                    {prod.options[0].price}
+                  <p className='tw-text-lg tw-font-semibold tw-mb-0 tw-text-right'>
+                    {prod.options[0]?.price}
                   </p>
-                  <p className=" tw-text-[#BFA5A3] tw-text-xs">
+                  <p className=' tw-text-[#BFA5A3] tw-text-xs'>
                     ({prod?.productRating.length} reviews)
                   </p>
                 </div>
@@ -103,25 +95,25 @@ const ProductFilled = function ({
             </div>
           ))}
         </div>
-        <div className="tw-mt-4">
+        <div className='tw-mt-4'>
           <ReactPaginate
-            nextLabel="next >"
-            onPageChange={(e) => handlePageClick(e)}
+            nextLabel='next >'
+            onPageChange={e => handlePageClick(e)}
             pageRangeDisplayed={3}
             marginPagesDisplayed={2}
             pageCount={pageCount}
-            previousLabel="< previous"
-            pageClassName="page-item"
-            pageLinkClassName="page-link"
-            previousClassName="page-item"
-            previousLinkClassName="page-link"
-            nextClassName="page-item"
-            nextLinkClassName="page-link"
-            breakLabel="..."
-            breakClassName="page-item"
-            breakLinkClassName="page-link"
-            containerClassName="pagination"
-            activeClassName="active"
+            previousLabel='< previous'
+            pageClassName='page-item'
+            pageLinkClassName='page-link'
+            previousClassName='page-item'
+            previousLinkClassName='page-link'
+            nextClassName='page-item'
+            nextLinkClassName='page-link'
+            breakLabel='...'
+            breakClassName='page-item'
+            breakLinkClassName='page-link'
+            containerClassName='pagination'
+            activeClassName='active'
             renderOnZeroPageCount={undefined}
           />
         </div>
