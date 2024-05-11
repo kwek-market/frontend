7706e31c3dfa1d4c39f58cf323e51bccce8e1f05
kingsley-sunny <ezeobisunny51@gmail.com>
2024-05-12 00:30:54 +0100

    fix: Made the categories icon dynamic

--- components/home/categoryMenu/CategoryMenu.tsx
+++ components/home/categoryMenu/CategoryMenu.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-import Link from "next/link";
+import { RootState } from "@/store/rootReducer";
 import Image from "next/image";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { v4 as uuid } from "uuid";
 import styles from "./CategoryMenu.module.scss";
 import CategoryMobile from "./CategoryMobile";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/rootReducer";
-import { v4 as uuid } from "uuid";
 
 const CategoryMenu = function () {
   const [showCat, setShowCat] = useState<boolean>(false);
@@ -19,29 +19,23 @@ const CategoryMenu = function () {
         <div className={styles.category_menuTitle}>
           <i className={`fas fa-bars ${styles.burger_icon}`} />
           <h4 className={styles.category_menuHeading}>All Categories</h4>
-          <div
-            className={styles.category_showMore_icon}
-            onClick={() => setShowCat(!showCat)}
-          >
+          <div className={styles.category_showMore_icon} onClick={() => setShowCat(!showCat)}>
             <i
               className={`fas fa-chevron-right ${styles.chevron_down}`}
               style={{ transform: showCat && "rotate(-90deg)" }}
             />
           </div>
         </div>
-        <ul
-          className={styles.category_list}
-          style={{ maxHeight: showCat && "325px" }}
-        >
-          {categories.categories.slice(0, 8).map((item) => (
+        <ul className={styles.category_list} style={{ maxHeight: showCat && "325px" }}>
+          {categories.categories.slice(0, 8).map(item => (
             <li key={uuid()} className={styles.menu_item}>
               <Link href={`/category/${item.name}`}>
                 <a className={styles.menu_link}>
                   <Image
                     src={item.icon || "/svg/cat-icon-electronics.svg"}
-                    width="24"
-                    height="24"
-                    className="tw-flex-[0.2]"
+                    width='24'
+                    height='24'
+                    className='tw-flex-[0.2]'
                   />
                   <span className={styles.menu_text}> {item.name} </span>
                 </a>
@@ -50,16 +44,12 @@ const CategoryMenu = function () {
           ))}
         </ul>
       </section>
-      <section className="tw-flex md:tw-hidden tw-overflow-x-auto tw-overflow-y-hidden">
-        <CategoryMobile
-          imgSrc="/svg/all.svg"
-          text="All"
-          style="tw-max-w-none"
-        />
-        {categories.categories.slice(0, 8).map((item) => (
+      <section className='tw-flex md:tw-hidden tw-overflow-x-auto tw-overflow-y-hidden'>
+        <CategoryMobile imgSrc='/svg/all.svg' text='All' style='tw-max-w-none' />
+        {categories.categories.slice(0, 8).map(item => (
           <CategoryMobile
             key={uuid()}
-            imgSrc={"/svg/cat-icon-electronics.svg"}
+            imgSrc={item.icon || "/svg/cat-icon-electronics.svg"}
             text={item.name}
           />
         ))}
