09f873326e1facc896c6389d5b060d03488f1019
kingsley-sunny <ezeobisunny51@gmail.com>
2024-05-17 19:01:17 +0100

    feature: implemented the functionality for a vendor to add many colors

--- components/singleProduct/SingleProduct.tsx
+++ components/singleProduct/SingleProduct.tsx
@@ -1,23 +1,20 @@
-import React from "react";
+import useAvgRating from "@/hooks/useAvgRating";
+import { ProductType } from "@/interfaces/commonTypes";
+import { message } from "antd";
 import Image from "next/image";
-import Link from "next/link";
-import styles from "./SingleProduct.module.scss";
 import { useRouter } from "next/router";
+import React from "react";
 import StarRatingComponent from "react-star-rating-component";
 import { v4 } from "uuid";
-import { ProductType } from "@/interfaces/commonTypes";
-import useAvgRating from "@/hooks/useAvgRating";
-import { message } from "antd";
+import { separateWords } from "../../helpers/helper";
+import styles from "./SingleProduct.module.scss";
 
 type SingleProductProps = {
   product: ProductType;
   setShowProduct: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const SingleProduct = function ({
-  product,
-  setShowProduct,
-}: SingleProductProps) {
+const SingleProduct = function ({ product, setShowProduct }: SingleProductProps) {
   const router = useRouter();
 
   function promote(id: string) {
@@ -43,19 +40,19 @@ const SingleProduct = function ({
       <div className={styles.holder}>
         <div className={styles.key}>
           <i
-            className="fas fa-arrow-left tw-cursor-pointer"
+            className='fas fa-arrow-left tw-cursor-pointer'
             onClick={() => setShowProduct(false)}
           />
         </div>
         <div className={styles.greenly}>
           <button
-            className="tw-bg-green-success tw-text-white-100 tw-rounded-sm tw-py-2 tw-px-3"
+            className='tw-bg-green-success tw-text-white-100 tw-rounded-sm tw-py-2 tw-px-3'
             onClick={() => promote(product.id)}
           >
             Promote
           </button>
           <button
-            className="tw-border-2 tw-border-gray-kwek900 tw-rounded-sm tw-py-2 tw-px-3"
+            className='tw-border-2 tw-border-gray-kwek900 tw-rounded-sm tw-py-2 tw-px-3'
             onClick={() => edit()}
           >
             Edit
@@ -67,18 +64,18 @@ const SingleProduct = function ({
           <div className={styles.mask}>
             <Image
               src={product.image[0].imageUrl}
-              width="722"
-              height="693.41"
+              width='722'
+              height='693.41'
               className={styles.img}
             />
           </div>
           <div className={styles.masky}>
-            {product.image.map((image) => (
+            {product.image.map(image => (
               <Image
                 key={v4()}
                 src={image.imageUrl}
-                width="163.22"
-                height="156.54"
+                width='163.22'
+                height='156.54'
                 className={styles.img}
               />
             ))}
@@ -89,22 +86,20 @@ const SingleProduct = function ({
           <div>
             <h4>{product.productTitle}</h4>
             <div className={styles.review}>
-              <div className="tw-flex-1">
+              <div className='tw-flex-1'>
                 <strong>Product Code:</strong> {product.id}
               </div>
 
               <div className={styles.revs}>
                 <StarRatingComponent
-                  name="seller-product"
+                  name='seller-product'
                   starCount={5}
                   value={useAvgRating(product)}
                   editing={false}
-                  emptyStarColor="#c4c4c4"
-                  starColor="#ffc107"
+                  emptyStarColor='#c4c4c4'
+                  starColor='#ffc107'
                 />
-                <p className={styles.six}>
-                  ({product?.productRating?.length} Reviews)
-                </p>
+                <p className={styles.six}>({product?.productRating?.length} Reviews)</p>
               </div>
             </div>
           </div>
@@ -113,21 +108,16 @@ const SingleProduct = function ({
 
           <div className={styles.colors}>
             <p>COLOR: </p>
-            <div
-              style={{ backgroundColor: product.color }}
-              className={styles.paint}
-            >
-              <div
-                style={{ backgroundColor: product.color }}
-                className="tw-w-[32px] tw-h-[32px]"
-              >
+            {separateWords(product.color).map(color => (
+              <div key={v4()} style={{ backgroundColor: color }} className={styles.paint}>
+                <div style={{ backgroundColor: color }} className='tw-w-[32px] tw-h-[32px]'></div>
               </div>
-            </div>
+            ))}
           </div>
           <div className={styles.size}>
             <p>SIZE:</p>
             <div className={styles.fort}>
-              {product.options.map((option) => (
+              {product.options.map(option => (
                 <div key={v4()} className={styles.forty}>
                   {option.size}
                 </div>
@@ -139,35 +129,35 @@ const SingleProduct = function ({
 
           <div className={styles.share}>
             <p className={styles.shareon}>Share on:</p>
-            <div className="tw-grid tw-grid-cols-2 lg:tw-grid-cols-1 tw-justify-center tw-items-center tw-gap-2 tw-ml-2">
+            <div className='tw-grid tw-grid-cols-2 lg:tw-grid-cols-1 tw-justify-center tw-items-center tw-gap-2 tw-ml-2'>
               <a
                 href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
-                target="_blank noreferer noopener"
-                className="tw-bg-[#3b5998] tw-text-white-100 tw-rounded-md tw-p-2 tw-truncate"
+                target='_blank noreferer noopener'
+                className='tw-bg-[#3b5998] tw-text-white-100 tw-rounded-md tw-p-2 tw-truncate'
               >
-                <i className="fab fa-facebook-f tw-mr-2" />
+                <i className='fab fa-facebook-f tw-mr-2' />
                 Facebook
               </a>
               <a
                 href={`https://twitter.com/intent/tweet?url=${window.location.href}`}
-                target="_blank noreferer noopener"
-                className="tw-bg-[#1da1f2] tw-text-white-100 tw-rounded-md tw-p-2"
+                target='_blank noreferer noopener'
+                className='tw-bg-[#1da1f2] tw-text-white-100 tw-rounded-md tw-p-2'
               >
-                <i className="fab fa-twitter tw-mr-2" />
+                <i className='fab fa-twitter tw-mr-2' />
                 Twitter
               </a>
               <button
-                className="tw-bg-[#25d366] tw-text-white-100 tw-rounded-md tw-p-2 tw-truncate"
+                className='tw-bg-[#25d366] tw-text-white-100 tw-rounded-md tw-p-2 tw-truncate'
                 onClick={() => shareOnWhatsapp()}
               >
-                <i className="fab fa-whatsapp tw-mr-2" />
+                <i className='fab fa-whatsapp tw-mr-2' />
                 Whatsapp
               </button>
               <button
-                className="tw-border tw-border-gray-kwek100 tw-text-gray-kwek100 tw-rounded-md tw-p-2 tw-truncate"
+                className='tw-border tw-border-gray-kwek100 tw-text-gray-kwek100 tw-rounded-md tw-p-2 tw-truncate'
                 onClick={() => copyLink()}
               >
-                <i className="fas fa-link tw-mr-2 " />
+                <i className='fas fa-link tw-mr-2 ' />
                 Copy Link
               </button>
             </div>
