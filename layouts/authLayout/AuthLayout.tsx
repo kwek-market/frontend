94b6df8de9bfb0ef239fc01a6dd9c14b5d15329a
kingsley-sunny <ezeobisunny51@gmail.com>
2024-05-26 16:01:24 +0100

    fix: fixed the login and the sign up layout

--- layouts/authLayout/AuthLayout.tsx
+++ layouts/authLayout/AuthLayout.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./AuthLayout.module.scss";
 
-import { Topbar } from "@/shared";
 import { useRouter } from "next/router";
 
 const AuthLayout = function ({
@@ -26,34 +23,33 @@ const AuthLayout = function ({
         {withBanner && (
           <div className={styles.banner}>
             {withLogo && (
-              <Link href="/">
+              <Link href='/'>
                 <a className={styles.logo}>
                   <Image
-                    src="/svg/kwek-logo-white.svg"
-                    width="150"
-                    height="25"
-                    placeholder="blur"
+                    src='/svg/kwek-logo-white.svg'
+                    width='150'
+                    height='25'
+                    placeholder='blur'
                   />
                 </a>
               </Link>
             )}
             <h1 className={styles.banner_text}>
-              {bannerText.lineOne} <br /> {bannerText.lineTwo} <br />{" "}
-              {bannerText.lineThree}
+              {bannerText.lineOne} <br /> {bannerText.lineTwo} <br /> {bannerText.lineThree}
             </h1>
             {withSubText && <h2 className={styles.subText}>{subText}</h2>}
             {bannerLink && (
               <div className={styles.banner_link}>
-                <Link href="#0">
+                <Link href='#0'>
                   <a className={styles.banner_link}>
                     <button className={`btn ${styles.btn}`}>
                       <div className={styles.btn_text}>Shop with Kwek</div>
                       <Image
                         className={styles.btn_icon}
-                        src="/svg/arrow-right-red.svg"
-                        width="24"
-                        height="11.6"
-                        placeholder="blur"
+                        src='/svg/arrow-right-red.svg'
+                        width='24'
+                        height='11.6'
+                        placeholder='blur'
                       />
                     </button>
                   </a>
@@ -64,15 +60,15 @@ const AuthLayout = function ({
         )}
 
         <div
-          className={
+          className={`${
             withBanner ? styles.formContainer : styles.formContainer__full
-          }
+          } tw-relative`}
         >
-          <button className="" onClick={() => router.push("/")}>
-            <i className="fas fa-arrow-left tw-text-left tw-text-lg" />
+          <button className='tw-absolute tw-top-10 tw-left-10' onClick={() => router.push("/")}>
+            <i className='fas fa-arrow-left tw-text-left tw-text-2xl' />
           </button>
 
-          {children}
+          <div className='tw-my-auto tw-w-full'>{children}</div>
         </div>
       </div>
     </div>
