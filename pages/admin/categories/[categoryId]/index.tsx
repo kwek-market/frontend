0fd60cb7d48137d5b8a3b714514b3aa32c509f0c
kingsley-sunny <ezeobisunny51@gmail.com>
2024-06-05 18:53:09 +0100

    fix: fix category links in admin page

--- pages/admin/categories/[categoryId]/index.tsx
+++ pages/admin/categories/[categoryId]/index.tsx
@@ -31,14 +31,14 @@ const Category = () => {
       <BreadCrumbs
         items={[
           { name: "Dashboard", path: "/admin/dashboard" },
-          { name: "Categories", path: "/admin/categories" },
+          { name: "Categories", path: "/admin/categories/category-list" },
           {
             name: router.query?.id as string,
             path: "/admin/categories/" + router.query?.id,
           },
         ]}
         header='Category Details'
-        buttonPath='/admin/category-list'
+        buttonPath='/admin/categories/category-list'
         buttonText='Back'
       />
 
