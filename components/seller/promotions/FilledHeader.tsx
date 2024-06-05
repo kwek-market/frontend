ec9931699d278a3c5dd2c2a79c7ed106eeb8e638
kingsley-sunny <ezeobisunny51@gmail.com>
2024-06-05 16:45:45 +0100

    added wallet system to the page, but await backen d issue

--- components/seller/promotions/FilledHeader.tsx
+++ components/seller/promotions/FilledHeader.tsx
@@ -1,19 +1,21 @@
+import { Modal, message } from "antd";
 import { useRouter } from "next/router";
-import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import usePayment from "../../../hooks/usePayment";
+import { RootState } from "../../../store/rootReducer";
 
 export default function FilledHeader() {
   const router = useRouter();
-
+  
   return (
-    <div className="tw-flex tw-justify-between ">
-      <h1 className="tw-font-semibold tw-text-lg tw-text-gray-kwek900">
-        Promotions
-      </h1>
+    <div className='tw-flex tw-justify-between '>
+      <h1 className='tw-font-semibold tw-text-lg tw-text-gray-kwek900'>Promotions</h1>
       <button
-        className="tw-rounded-md tw-capitalize tw-bg-red-kwek100 tw-text-white-100 tw-p-3 tw-flex tw-items-center"
+        className='tw-rounded-md tw-capitalize tw-bg-red-kwek100 tw-text-white-100 tw-p-3 tw-flex tw-items-center'
         onClick={() => router.push("/seller/profile")}
       >
-        <i className="fas fa-plus tw-mr-2" /> new promotion
+        <i className='fas fa-plus tw-mr-2' /> new promotion
       </button>
     </div>
   );
