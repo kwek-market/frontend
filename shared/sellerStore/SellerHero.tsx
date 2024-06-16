80650e05980dd4fcba1ce1c9a736ea1eb50e7106
kingsley-sunny <ezeobisunny51@gmail.com>
2024-06-16 02:18:05 +0100

    feature: designed the seller store

---
+++ shared/sellerStore/SellerHero.tsx
@@ -0,0 +1,93 @@
+import Button from "@/components/buttons/Button";
+import useReviews from "@/hooks/useReviews";
+import { RootState } from "@/store/rootReducer";
+import { Rate } from "antd";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Container } from "../../components/seller/content/Content";
+
+export const SellerHero = () => {
+  const { seller, user } = useSelector((state: RootState) => state);
+  const router = useRouter();
+
+  const { status, data, error } = useReviews({
+    token: user.token,
+    page: 1,
+    pageSize: 20,
+  });
+
+  const bgImg = seller.seller.storeBannerUrl
+    ? `linear-gradient(rgba(87, 66, 64, 0.7), rgba(87, 66, 64, 0.7)), url('${seller.seller.storeBannerUrl}')`
+    : "linear-gradient(rgba(87, 66, 64, 0.7), rgba(87, 66, 64, 0.7)), url('/images/stairs.jpg')";
+
+  function uploadHandler() {
+    router.push("/seller/upload-new-product");
+  }
+
+  const rating = useMemo(() => {
+    if (data) {
+      const ratings = data.getSellerReview.objects.map(
+        (review: { rating: number }) => review.rating
+      );
+      const total = ratings.reduce((acc, cur) => acc + cur, 0);
+      const avg = total / ratings.length;
+      return avg;
+    }
+    return 0;
+  }, [data]);
+
+  return (
+    <div
+      style={{
+        background: bgImg,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundBlendMode: "multiply",
+        backgroundColor: "gray",
+      }}
+      className='tw-bg-seller-header tw-bg-no-repeat tw-bg-center tw-bg-cover'
+    >
+      <Container>
+        <div className='tw-p-2 md: tw-py-7 md:tw-px-7 tw-flex tw-justify-between tw-items-center'>
+          <div className='tw-flex tw-flex-col md:tw-flex-row'>
+            <div className='md:tw-mr-4'>
+              <img
+                src={
+                  seller.seller.storeBannerUrl
+                    ? seller.seller.storeBannerUrl
+                    : "/images/user-photo.svg"
+                }
+                className='tw-rounded-xl '
+                width={"150px"}
+              />
+            </div>
+            <div className='tw-self-end tw-mb-4'>
+              <p className='tw-font-semibold tw-text-white-100 tw-text-4xl tw-mb-0'>
+                {seller.seller.shopName}
+              </p>
+              <p className='tw-text-white-200'>Clothings for all ages.</p>
+
+              <div className='tw-text-md'>
+                <Rate disabled allowHalf value={rating} className='tw-text-[12px]' />
+                {data !== undefined && data.getSellerReview.objects.length > 0 ? (
+                  <span className='tw-text-white-100 tw-text-[12px]'>({rating} reviews)</span>
+                ) : (
+                  <span className='tw-text-white-100 tw-text-[12px]'>(0 reviews)</span>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className='tw-self-end tw-mb-7'>
+            <Button
+              icon={"fa-plus"}
+              buttonStyle={"tw-rounded-sm tw-p-3 tw-bg-yellow-filled hover:tw-shadow-md"}
+              text={"Contact Us"}
+              cmd={uploadHandler}
+            />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
