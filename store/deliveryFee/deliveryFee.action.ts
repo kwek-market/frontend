3ce56e0a450600d1ac8e93d3a3b0a528084369d6
kingsley-sunny <ezeobisunny51@gmail.com>
2024-06-22 12:19:28 +0100

    fixed: fixed state and delivery with order on the user side

--- store/deliveryFee/deliveryFee.action.ts
+++ store/deliveryFee/deliveryFee.action.ts
@@ -1,11 +1,12 @@
 import { DeliveryFeeEnum } from "./deliveryFee.types";
 
-export function setDeliveryFee(state: string, fee: number) {
+export function setDeliveryFee(state: string, fee: number, city?: string) {
   return {
     type: DeliveryFeeEnum.SET_DELIVERY_FEE,
     payload: {
       state,
       fee,
+      city,
     },
   };
 }
