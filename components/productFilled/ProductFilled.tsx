a0b80b434af9e9d76e9ad2b62793666836700552
kingsley-sunny <ezeobisunny51@gmail.com>
2024-06-22 14:24:48 +0100

    fixed: fixed the user store page

--- components/productFilled/ProductFilled.tsx
+++ components/productFilled/ProductFilled.tsx
@@ -4,6 +4,7 @@ import { ProductType } from "@/interfaces/commonTypes";
 import { RootState } from "@/store/rootReducer";
 import { Rate } from "antd";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import ReactPaginate from "react-paginate";
 import { useSelector } from "react-redux";
@@ -37,11 +38,6 @@ const ProductFilled = function ({
     return data?.getSellerPromotedProducts.find(item => item.id === id);
   }
 
-  function displayProductDetails(prod: any) {
-    setShowProduct(true);
-    setProduct(prod);
-  }
-
   return (
     <section className='tw-shadow-lg'>
       <div className={styles.filled}>
@@ -60,39 +56,37 @@ const ProductFilled = function ({
         </div>
         <div className='tw-grid tw-gap-3 tw-grid-cols-2 lg:tw-grid-cols-4 tw-mt-2'>
           {product.map((prod: ProductType) => (
-            <div
-              key={prod.id}
-              className='tw-shadow-lg tw-cursor-pointer tw-relative'
-              onClick={() => displayProductDetails(prod)}
-            >
-              <Image
-                src={prod.image[0].imageUrl}
-                alt={prod.productTitle}
-                layout='responsive'
-                width={200}
-                height={200}
-                className=' tw-object-cover'
-              />
-              {!!isPromoted(prod.id) && (
-                <span className='tw-bg-green-success tw-text-white-100 tw-rounded-xl tw-py-1 tw-px-4 tw-absolute tw-left-[35%] md:tw-left-[60%] tw-top-[5%]'>
-                  promoted
-                </span>
-              )}
-              <div className='tw-flex tw-flex-wrap tw-justify-between tw-p-3'>
-                <div>
-                  <p className='tw-text-lg tw-font-semibold tw-mb-0'>{prod.productTitle}</p>
-                  <Rate allowHalf value={useAvgRating(prod)} />
+            <Link href={`/product/${prod?.id}`}>
+              <a key={prod.id} className='tw-shadow-lg tw-cursor-pointer tw-relative tw-block'>
+                <Image
+                  src={prod.image[0].imageUrl}
+                  alt={prod.productTitle}
+                  layout='responsive'
+                  width={200}
+                  height={200}
+                  className=' tw-object-cover'
+                />
+                {!!isPromoted(prod.id) && (
+                  <span className='tw-bg-green-success tw-text-white-100 tw-rounded-xl tw-py-1 tw-px-4 tw-absolute tw-left-[35%] md:tw-left-[60%] tw-top-[5%]'>
+                    promoted
+                  </span>
+                )}
+                <div className='tw-flex tw-flex-wrap tw-justify-between tw-p-3'>
+                  <div>
+                    <p className='tw-text-lg tw-font-semibold tw-mb-0'>{prod.productTitle}</p>
+                    <Rate allowHalf value={useAvgRating(prod)} />
+                  </div>
+                  <div>
+                    <p className='tw-text-lg tw-font-semibold tw-mb-0 tw-text-right'>
+                      {prod.options[0]?.price}
+                    </p>
+                    <p className=' tw-text-[#BFA5A3] tw-text-xs'>
+                      ({prod?.productRating.length} reviews)
+                    </p>
+                  </div>
                 </div>
-                <div>
-                  <p className='tw-text-lg tw-font-semibold tw-mb-0 tw-text-right'>
-                    {prod.options[0]?.price}
-                  </p>
-                  <p className=' tw-text-[#BFA5A3] tw-text-xs'>
-                    ({prod?.productRating.length} reviews)
-                  </p>
-                </div>
-              </div>
-            </div>
+              </a>
+            </Link>
           ))}
         </div>
         <div className='tw-mt-4'>
