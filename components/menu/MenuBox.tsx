cd32e6758ab54337e21d1d04554fb069dd2fcd20
kingsley-sunny <ezeobisunny51@gmail.com>
2024-06-23 02:04:38 +0100

    fixed: fixed view your store menu on the homepage

--- components/menu/MenuBox.tsx
+++ components/menu/MenuBox.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import Link from 'next/link';
-import style from './menubox.module.scss';
+import Link from "next/link";
+import style from "./menubox.module.scss";
 
 type MenuBoxProps = {
   icon: string;
   title: string;
   description: string;
   link: string;
+  show?: boolean;
 };
 
-const MenuBox = function ({ icon, title, description, link }: MenuBoxProps) {
+const MenuBox = function ({ icon, title, description, link, show }: MenuBoxProps) {
   return (
-    <Link href={link}>
-      <a>
-        <span className={style.menuBox}>
-          <span>
-            <i className={`fas ${icon} icon tw-text-gray-kwek900`} />
-          </span>
-          <span style={{ paddingLeft: '12px' }}>
-            <span className={style.title}>{title}</span>
-            <span className={style.description}>{description}</span>
-          </span>
-        </span>
-      </a>
-    </Link>
+    <>
+      {show ? (
+        <Link href={link}>
+          <a>
+            <span className={style.menuBox}>
+              <span>
+                <i className={`fas ${icon} icon tw-text-gray-kwek900`} />
+              </span>
+              <span style={{ paddingLeft: "12px" }}>
+                <span className={style.title}>{title}</span>
+                <span className={style.description}>{description}</span>
+              </span>
+            </span>
+          </a>
+        </Link>
+      ) : null}
+    </>
   );
 };
 
