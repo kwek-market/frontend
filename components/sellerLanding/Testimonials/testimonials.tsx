6536c0218e2573f33bed4524aff5c7e2f9269d13
kingsley-sunny <ezeobisunny51@gmail.com>
2024-10-18 10:53:03 +0100

    fixed: fixed testimonials

--- components/sellerLanding/Testimonials/testimonials.tsx
+++ components/sellerLanding/Testimonials/testimonials.tsx
@@ -9,15 +9,10 @@ const Testimonials = () => {
       element: (
         <>
           <div className='tw-flex tw-relative'>
-            <Image
-              className='circle-image'
-              src='/images/testimonial-man.png'
-              height='70'
-              width='70'
-            />
+            <Image className='circle-image' src='/images/james.jpg' height='70' width='70' />
             <div className='tw-flex tw-flex-col tw-justify-center tw-items-left tw-ml-3'>
-              <h3 className='tw-text-base md:tw-text-lg tw-text-gray-kwek200'>James Mathew</h3>
-              <h4 className='tw-text-sm md:tw-text-lg'>Customer</h4>
+              <h3 className='tw-text-base md:tw-text-lg tw-text-gray-kwek200'>James AFUYE</h3>
+              <h4 className='tw-text-sm md:tw-text-lg'>Sustainable Living Advocate</h4>
             </div>
             <div className='tw-absolute tw-right-1'>
               <img src='/images/quote.png' className='tw-w-10' />
@@ -25,9 +20,10 @@ const Testimonials = () => {
           </div>
           <div>
             <h3 className='  xl:tw-line-clamp-none tw-text-gray-kwek200 tw-opacity-70 tw-text-sm md:tw-text-lg tw-mt-6'>
-              &ldquo;Great experience shopping with you! The process was smooth, delivery was fast,
-              and my order arrived exactly as described. I will definitely shop here again and
-              recommend to others!&ldquo;
+              &ldquo;Kwekmarket has completely transformed my shopping experience! The variety of
+              unique, eco-friendly products is amazing, and I love supporting local vendors. Plus,
+              the quick delivery and smooth transactions make it so convenient. It's the perfect
+              marketplace for anyone who values sustainability and quality!&ldquo;
             </h3>
           </div>
           {/* <div className='tw-absolute tw-bottom-[5%]  md:tw-bottom-[10%] '>
@@ -47,8 +43,8 @@ const Testimonials = () => {
               width='70'
             />
             <div className='tw-flex tw-flex-col tw-justify-center tw-items-left tw-ml-3'>
-              <h3 className='tw-text-base md:tw-text-lg tw-text-gray-kwek200'>Grego</h3>
-              <h4 className='tw-text-sm md:tw-text-lg'>Developer</h4>
+              <h3 className='tw-text-base md:tw-text-lg tw-text-gray-kwek200'>Julian Akinremi</h3>
+              <h4 className='tw-text-sm md:tw-text-lg'>CEO Fourteen farms</h4>
             </div>
             <div className='tw-absolute tw-right-1'>
               <img src='/images/quote.png' className='tw-w-10' />
@@ -56,10 +52,10 @@ const Testimonials = () => {
           </div>
           <div>
             <h3 className='  xl:tw-line-clamp-none tw-text-gray-kwek200 tw-opacity-70 tw-text-sm md:tw-text-lg tw-mt-6'>
-              &ldquo;I had a great experience shopping on this website. The layout is super easy to
-              navigate, and I found exactly what I was looking for without any hassle. The product
-              pages are detailed, and the photos make it easy to see what you’re getting, which is
-              always a plus.&ldquo;
+              &ldquo;Kwekmarket is a game-changer! I love how easy it is to find unique,
+              eco-friendly products while supporting local sellers. The delivery is super fast, and
+              the customer service is always on point. It's my go-to marketplace for stylish and
+              sustainable shopping!&ldquo;
             </h3>
           </div>
           {/* <div className='tw-absolute tw-bottom-[5%]  md:tw-bottom-[10%]'>
